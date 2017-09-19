9d66d3999b7a44470cff168656667207fe8dbafb
The-CJ <christoperjaekel@googlemail.com>
2017-09-19 23:04:52 +0200

    Added Style and Swag

--- debug.py
+++ debug.py
@@ -8,24 +8,67 @@ class Deb():
 		self.BASE = BASE
 		self.root = tk.Tk()
 
-		self.root.title("Test")
-		#self.root.geometry("300x150")
+		self.root.title("PhaazeOS System Debug")
+
 		self.storage = None
+		self.last_operation = ""
+		self.async_operation = tk.IntVar()
+		self.output = tk.StringVar()
+		self.return_value = tk.StringVar()
 
 		self.frame = tk.Frame(self.root)
-		self.lable = tk.Label(self.root, text=">>>").pack(side=tk.LEFT)
-		self.entry = tk.Entry(self.root, textvariable=self.storage).pack(side=tk.RIGHT)
-		self.root.bind("<Return>", func=self.Return_)
+		self.lable_site = tk.Label(self.root, text="root@phaazeos >>>").grid(row=2, column=0, sticky="E")
+		self.lable_down = tk.Label(self.root, textvariable=self.output,justify='left').grid(row=0, column=1, sticky="W")
+		self.entry = tk.Entry(self.root, width=40, textvariable=self.storage, justify='left').grid(row=2, column=1, sticky="W")
+		self.result = tk.Label(self.root, textvariable=self.return_value, justify='left', fg="blue", wraplength=500).grid(row=3, column=1, sticky="W")
 
-		self.frame.pack()
+		self.async_box = tk.Checkbutton(self.root, text="Async operation:", variable=self.async_operation).grid(row=1, sticky="E")
+
+		self.root.bind("<Return>", func=self.Return_)
+		self.root.bind("<Up>", func=self.load_last)
 		self.root.mainloop()
 
-	def Return_(self, event):
-		print(event.widget.get())
+	def load_last(self, event):
 		event.widget.delete(0, 'end')
+		event.widget.insert(0, self.last_operation)
+
+	def Return_(self, event):
+		command = event.widget.get()
+		event.widget.width = 50
+		self.output.set(self.output.get() + "\n>> " + command)
+		self.output.set(self.output.get()[-200:])
+		event.widget.delete(0, 'end')
+
+		if command == "clear":
+			self.output.set("")
+			self.return_value.set("Cleared")
+			return
+
+		elif command == "shutdown":
+			loop = asyncio.new_event_loop()
+			loop.run_until_complete(self.BASE.shutdown(self.BASE))
+			return exit()
+
+		elif command == "reload":
+			loop = asyncio.new_event_loop()
+			loop.run_until_complete(self.BASE.utils.reload_(self.BASE))
+
+		try:
+			self.last_operation = command
+
+			if self.async_operation.get() == 1:
+				loop = asyncio.new_event_loop()
+				f = asyncio.ensure_future(eval(command))
+				self.result = tk.Label(self.root, textvariable=self.return_value, justify='left', fg="lime", wraplength=500).grid(row=3, column=1, sticky="W")
+			else:
+				f = eval(command)
+				self.result = tk.Label(self.root, textvariable=self.return_value, justify='left', fg="blue", wraplength=500).grid(row=3, column=1, sticky="W")
+
+			self.return_value.set(f)
+		except Exception as Fail:
+			self.result = tk.Label(self.root, textvariable=self.return_value, justify='left', fg="red", wraplength=500).grid(row=3, column=1, sticky="W")
+			self.return_value.set(str(Fail))
+
 
 async def main(BASE):
 	Deb(BASE)
-
-l = asyncio.new_event_loop()
-l.run_until_complete(main())
