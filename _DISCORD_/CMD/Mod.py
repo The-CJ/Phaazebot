29b9f35afa194405e3c7d7f0f756abe13733b0d0
The-CJ <christoperjaekel@googlemail.com>
2018-04-02 17:16:05 +0200

    mod commands can be disabled

--- _DISCORD_/CMD/Mod.py
+++ _DISCORD_/CMD/Mod.py
@@ -2,10 +2,30 @@
 
 import asyncio
 
-async def base(BASE, message):
+async def no_mod(BASE, message, kwargs):
+	m = await BASE.phaaze.send_message(message.channel, ":no_entry_sign: You can not use Mod Commands")
+	await asyncio.sleep(2.5)
+	await BASE.phaaze.delete_message(m)
+
+async def owner_disabled_mod(BASE, message, kwargs):
+	m = await BASE.phaaze.send_message(message.channel, ":no_entry_sign: The Serverowner disabled Mod-commands, only the Serverowner can use them")
+	await asyncio.sleep(2.5)
+	await BASE.phaaze.delete_message(m)
+
+async def Base(BASE, message, **kwargs):
+	if not await BASE.moduls._Discord_.Utils.is_Mod(BASE, message):
+		asyncio.ensure_future(no_mod(BASE, message, kwargs))
+		return
+
+	if not BASE.moduls._Discord_.Utils.is_Owner(BASE, message) and kwargs.get('server_setting', {}).get('owner_disable_mod', False):
+		asyncio.ensure_future(owner_disabled_mod(BASE, message, kwargs))
+		return
+
 	m = message.content.lower().split(" ")
 	check = m[0][2:]
 
+	if check.startswith("setting"):
+		await BASE.moduls_Discord_.PROCESS.Mod.Settings.Base(BASE, message, kwargs)
 
 	if check.startswith("addcom"):
 		if await BASE.moduls.Utils.is_Mod(BASE, message):
@@ -22,12 +42,6 @@ async def base(BASE, message):
 	if check.startswith("blacklist"):
 		if await BASE.moduls.Utils.is_Mod(BASE, message):
 			await BASE.moduls.Blacklist.base(BASE, message)
-		else:
-			await BASE.moduls.Utils.no_mod(BASE, message)
-
-	if check.startswith("setting"):
-		if await BASE.moduls.Utils.is_Mod(BASE, message):
-			await BASE.moduls.Mod_Commands.settings(BASE, message)
 		else:
 			await BASE.moduls.Utils.no_mod(BASE, message)
 
