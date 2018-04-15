fecf59bf32da5a683798c4a73f52254c24246e39
Byte <HeapUnderfl0w@users.noreply.github.com>
2018-04-15 15:33:54 +0200

    Fixed naming and used better Queue
    Fixed the naming of the global variable to be more conform with PEP-8  
    Also changed the list to a deque do avoid reordering the list in memory on each print.

--- Console.py
+++ Console.py
@@ -1,5 +1,8 @@
 from colorama import Fore, Back, Style, init
+from collections import deque
 import asyncio, datetime
+
+# Colorama Console INIT
 init()
 
 WRITE = True
@@ -10,14 +13,13 @@ WRITE = True
 #Back: 		BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 #Style: 	DIM, NORMAL, BRIGHT, RESET_ALL
 
-Save_print = []
+SAFE_PRINT = deque([])
 async def Debug_and_status_console(BASE):
 	"""Thread save Debug Console"""
 
 	while BASE.active.debug_console:
-		if len(Save_print) > 0:
-			req = Save_print[0]
-			Save_print.remove(Save_print[0])
+		if len(SAFE_PRINT) > 0:
+			req = SAFE_PRINT.popleft()
 			try:
 				now = datetime.datetime.now()
 				time_ = "["+now.strftime("%H:%M:%S")+"]"
@@ -29,25 +31,25 @@ async def Debug_and_status_console(BASE):
 	print("[INFO] Debug Console has been shutdown")
 
 def WHITE(a, m):
-	Save_print.append("[" + a + Style.RESET_ALL + "]  " + m)
+	SAFE_PRINT.append("[" + a + Style.RESET_ALL + "]  " + m)
 
 def BLACK(a, m):
-	Save_print.append("[" + Fore.BLACK + a + Style.RESET_ALL + "]  " + m)
+	SAFE_PRINT.append("[" + Fore.BLACK + a + Style.RESET_ALL + "]  " + m)
 
 def RED(a, m):
-	Save_print.append("[" + Fore.RED + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.RED + a + Style.RESET_ALL + "] " + m)
 
 def GREEN(a, m):
-	Save_print.append("[" + Fore.GREEN + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.GREEN + a + Style.RESET_ALL + "] " + m)
 
 def YELLOW(a, m):
-	Save_print.append("[" + Fore.YELLOW + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.YELLOW + a + Style.RESET_ALL + "] " + m)
 
 def BLUE(a, m):
-	Save_print.append("[" + Fore.BLUE + Style.BRIGHT + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.BLUE + Style.BRIGHT + a + Style.RESET_ALL + "] " + m)
 
 def MAGENTA(a, m):
-	Save_print.append("[" + Fore.MAGENTA + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.MAGENTA + a + Style.RESET_ALL + "] " + m)
 
 def CYAN(a, m):
-	Save_print.append("[" + Fore.CYAN + a + Style.RESET_ALL + "] " + m)
+	SAFE_PRINT.append("[" + Fore.CYAN + a + Style.RESET_ALL + "] " + m)
