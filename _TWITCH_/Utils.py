3eb7cf2a9a69312b51c2f5356bbdb95b3adb0d04
The-CJ <christoperjaekel@googlemail.com>
2018-05-12 16:58:08 +0200

    made Utils equal to Discord

--- _TWITCH_/Utils.py
+++ _TWITCH_/Utils.py
@@ -1,137 +1,122 @@
 #BASE.modules._Twitch_.Utils
 
-import asyncio, json, requests
+import asyncio, requests
 
 MAIN = "https://api.twitch.tv/kraken/"
 
-#settings
-async def get_twitch_file(BASE, room_id):
-	#has file in lib
-	if hasattr(BASE.twitchfiles, "channel_"+room_id):
-		file = getattr(BASE.twitchfiles, "channel_"+room_id)
-		return file
+#auth
+async def is_Mod(BASE, message):
+	if message.mod: return True
+	if message.user_type.lower() == "mod": return True
+	if message.user_type.lower() == "global_mod": return True
+	if message.user_type.lower() == "admin": return True
+	if message.user_type.lower() == "staff": return True
 
-	try:
-		file = open("_TWITCH_/Channel_files/{0}.json".format(room_id), "r")
-		file = file.read()
-		file = json.loads(file)
+	if message.name == message.channel.name: return True
 
-		#add to lib
-		setattr(BASE.twitchfiles, "channel_"+room_id, file)
+	return False
 
-		return file
+async def is_Owner(BASE, message):
+	if message.user_type.lower() == "admin": return True
+	if message.user_type.lower() == "staff": return True
+	if message.name == message.channel.name: return True
 
-	#none found, make new
-	except FileNotFoundError:
+	return False
 
-		file = await make_twitch_file(BASE, room_id)
-		return file
+#channelfiles
+async def get_channel_settings(BASE, id, prevent_new=False):
+	#get
+	file = BASE.PhaazeDB.select(of="twitch/channel_settings", where="data['channel_id'] == '{}'".format(id))
 
-	#json bullsahit
-	except json.decoder.JSONDecodeError:
-		BASE.modules.Console.RED("CRITICAL ERROR", "Broken json twitch channel file")
+	if len(file['data']) == 0:
+		#didn't find entry -> make new
+		if prevent_new:
+			return None
+		else:
+			return await make_channel_settings(BASE, id)
+	else:
+		return file['data'][0]
 
-	#something new
-	except Exception as e:
-		print(str(e.__class__) + "in File")
+async def make_channel_settings(BASE, id):
+	insert_ = dict()
 
-async def make_twitch_file(BASE, room_id):
-	struktur = {"room_id": room_id}
-	with open("_TWITCH_/Channel_files/{0}.json".format(room_id), "w") as new:
-		json.dump(struktur, new)
+	insert_['channel_id'] = str(id)
 
-	BASE.modules.Console.CYAN("INFO", "New Twitch Channel created")
+	insert_['active_quotes'] = False
+	insert_['active_custom'] = True
+	insert_['active_games'] = False
+	insert_['ban_links'] = False
+	insert_['regulars'] = []
+	insert_['blacklist_punishment'] = None
+	insert_['blacklist'] = []
 
-	file = open("_TWITCH_/Channel_files/{0}.json".format(room_id), "r")
-	file = file.read()
-	file = json.loads(file)
+	BASE.PhaazeDB.insert(into="twitch/channel_settings", content=insert_)
+	BASE.modules.Console.CYAN("INFO", "New Twitch Setting DB entry")
 
-	return file
+	return insert_
 
-#level
-async def get_twitch_level_file_(BASE, room_id):
-	#has file in lib
-	if hasattr(BASE.twitchlevelfiles, "channel_"+room_id):
-		file = getattr(BASE.twitchlevelfiles, "channel_"+room_id)
-		return file
+#customfiles
+async def get_channel_commands(BASE, id, prevent_new=False):
+	#get
+	file = BASE.PhaazeDB.select(of="twitch/commands/commands_"+str(id))
 
-	try:
-		file = open("_TWITCH_/Channel_level_files/{0}.json".format(room_id), "r")
-		file = file.read()
-		file = json.loads(file)
+	if file['status'] == "error":
+		#didn't find entry -> make new
+		if prevent_new:
+			return None
+		else:
+			return await make_channel_commands(BASE, id)
+	else:
+		return file['data']
 
-		#add to lib
-		setattr(BASE.twitchlevelfiles, "channel_"+room_id, file)
+async def make_channel_commands(BASE, id):
 
-		return file
+	BASE.PhaazeDB.create(name="twitch/commands/commands_"+str(id))
+	BASE.modules.Console.CYAN("INFO", "New Twitch Channel Command DB-Container created")
 
-	#none found, make new
-	except FileNotFoundError:
+	return []
 
-		file = await make_twitch_level_file(BASE, room_id)
-		return file
+#levelfiles
+async def get_channel_levels(BASE, id, prevent_new=False):
+	#get
+	file = BASE.PhaazeDB.select(of="twitch/level/level_"+str(id))
 
-	#json bullsahit
-	except json.decoder.JSONDecodeError:
-		BASE.modules.Console.RED("CRITICAL ERROR", "Broken json twitch level file")
+	if file['status'] == "error":
+		#didn't find entry -> make new
+		if prevent_new:
+			return None
+		else:
+			return await make_channel_levels(BASE, id)
+	else:
+		return file['data']
 
-	#something new
-	except Exception as e:
-		print(str(e.__class__) + "in Level")
-#level
-async def get_twitch_level_file(BASE, room_id):
-	#has file in lib
-	if hasattr(BASE.twitchlevelfiles, "channel_"+room_id):
-		file = getattr(BASE.twitchlevelfiles, "channel_"+room_id)
-		return file
+async def make_channel_levels(BASE, id):
 
-	try:
-		file = open("_TWITCH_/Channel_level_files/{0}.json".format(room_id), "r")
-		file = file.read()
-		file = json.loads(file)
+	BASE.PhaazeDB.create(name="twitch/level/level_"+str(id))
+	BASE.modules.Console.CYAN("INFO", "New Twitch Channel Level DB-Container created")
 
-		#add to lib
-		setattr(BASE.twitchlevelfiles, "channel_"+room_id, file)
+	return []
 
-		return file
+#quotefiles
+async def get_channel_quotes(BASE, id, prevent_new=False):
+	#get
+	file = BASE.PhaazeDB.select(of="twitch/quotes/quotes_"+str(id))
 
-	#none found, make new
-	except FileNotFoundError:
+	if file['status'] == "error":
+		#didn't find entry -> make new
+		if prevent_new:
+			return None
+		else:
+			return await make_channel_quotes(BASE, id)
+	else:
+		return file['data']
 
-		file = await make_twitch_level_file(BASE, room_id)
-		return file
+async def make_channel_quotes(BASE, id):
 
-	#json bullsahit
-	except json.decoder.JSONDecodeError:
-		BASE.modules.Console.RED("CRITICAL ERROR", "Broken json twitch level file")
+	BASE.PhaazeDB.create(name="twitch/quotes/quotes_"+str(id))
+	BASE.modules.Console.CYAN("INFO", "New Twitch Quote Level DB-Container created")
 
-async def make_twitch_level_file(BASE, room_id):
-	struktur = {"room_id": room_id, "user": []}
-	with open("_TWITCH_/Channel_level_files/{0}.json".format(room_id), "w") as new:
-		json.dump(struktur, new)
-
-	BASE.modules.Console.CYAN("INFO", "New Twitch Level created")
-
-	file = open("_TWITCH_/Channel_level_files/{0}.json".format(room_id), "r")
-	file = file.read()
-	file = json.loads(file)
-
-	return file
-
-#channel objects
-async def get_channel_object(BASE, id=None, name=None):
-	found = None
-
-	if id != None:
-		for channel in BASE.Twitch_IRC_connection.channels:
-			if channel.room_id == id:
-				return channel
-
-	if name != None:
-		for channel in BASE.Twitch_IRC_connection.channels:
-			if channel.name.lower() == name.lower():
-				return channel
-	return found
+	return []
 
 # # #
 
