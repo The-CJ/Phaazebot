7aa77da25f6f79424262a02aee95de0996b909d6
The_CJ <christoperjaekel@googlemail.com>
2018-06-22 18:33:02 +0200

    added method so phaaze joins all channels on start, need to fix

--- _TWITCH_/Main_twitch.py
+++ _TWITCH_/Main_twitch.py
@@ -1,6 +1,6 @@
 #BASE.modules.Twitch_IRC
 
-import asyncio
+import asyncio, time
 import twitch_irc as twitch
 
 #BASE.twitch
@@ -10,15 +10,32 @@ class Init_twitch(twitch.Client):
 		super().__init__()
 
 	async def on_ready(self):
-		self.BASE.is_ready.twitch = True
 		self.BASE.modules.Console.GREEN("SUCCESS", "Twitch IRC Connected")
+
+		#join all channel
 		await self.join_channel(self.nickname)
+
+		await self.join_all()
+		self.BASE.is_ready.twitch = True
 
 	#message management
 	async def on_message(self, message):
 		if message.name.lower() == self.nickname.lower(): return
-
+		print('mes')
 		if not self.BASE.is_ready.twitch: return
 
 		await self.BASE.modules._Twitch_.Base.on_message(self.BASE, message)
 
+
+	async def join_all(self):
+		req = self.BASE.PhaazeDB.select(of="setting/twitch_channel")
+		data = req.get('data',[])
+
+		#join limit / 30sec
+		request_limit = 20
+
+		for channel in data:
+			channel_name = channel.get('twitch_name', None)
+			if channel_name == None: continue
+			await self.join_channel(channel_name)
+			await asyncio.sleep(request_limit / 32)
