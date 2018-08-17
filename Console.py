c578223bcd65695b93ae6bad7e9dcc4b01ece677
The_CJ <christoperjaekel@googlemail.com>
2018-08-17 14:27:47 +0200

    some small changes to logging, including systemd

--- Console.py
+++ Console.py
@@ -1,11 +1,26 @@
-import logging
+import logging, sys, re
+from systemd.journal import JournalHandler
+
+option_re = re.compile(r'^--(.+?)=(.*)$')
+all_args = dict()
+for arg in sys.argv[1:]:
+	d = option_re.match(arg)
+	if d != None:
+		all_args[d.group(1)] = d.group(2)
 
 LOG = logging.getLogger('PhaazeOS')
 LOG.setLevel(logging.DEBUG)
-SH = logging.StreamHandler()
+
 SHF = logging.Formatter("[%(levelname)s] %(message)s")
-SH.setFormatter(SHF)
-LOG.addHandler(SH)
+
+if all_args.get('logging', 'console') == "systemd":
+	JH = JournalHandler()
+	JH.setFormatter(SHF)
+	LOG.addHandler(JH)
+else:
+	SH = logging.StreamHandler()
+	SH.setFormatter(SHF)
+	LOG.addHandler(SH)
 
 def DEBUG(m):
 	LOG.debug(m)
