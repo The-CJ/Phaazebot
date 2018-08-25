8fca1120a9b742ac136d0639f86205176a410189
The_CJ <christoperjaekel@googlemail.com>
2018-08-25 16:57:59 +0200

    added mod quote add | !quote [index] for normales

--- _TWITCH_/PROCESS/Mod.py
+++ _TWITCH_/PROCESS/Mod.py
@@ -3,8 +3,30 @@
 import asyncio
 
 class Quote(object):
-	async def add(*a):
-		pass
+	async def add(BASE, message, kwargs):
+		m = message.content.split(" ")
 
-	async def rem(*a):
-		pass
+		if len(m) <= 1:
+			r = f"Error! > !addquote [Content]"
+			return await BASE.twitch.send_message(message.channel_name, r)
+
+		channel_quotes = await BASE.modules._Twitch_.Utils.get_channel_quotes(BASE, message.channel_id)
+
+		if len(channel_quotes) >= 100:
+			return await BASE.twitch.send_message(message.channel_name, "Error: There are allready 100 quotes, delete some first")
+
+		quote_content = " ".join(m[1:])
+
+		ins = BASE.PhaazeDB.insert(into=f"twitch/quotes/quotes_{message.channel_id}", content=dict(content=quote_content))
+
+		_id_ = ins.get('data', {}).get('id', '[N/A]')
+
+		return await BASE.twitch.send_message(message.channel_name, f'Quote successfull added, ID: "{str(_id_)}"')
+
+	async def rem(BASE, message, kwargs):
+		m = message.content.split(" ")
+
+		if len(m) <= 2:
+			r = f"Error! > !delquote [ID]"
+			return await BASE.twitch.send_message(message.channel_name, r)
+
--- _TWITCH_/PROCESS/Normal.py
+++ _TWITCH_/PROCESS/Normal.py
@@ -1,6 +1,6 @@
 #BASE.modules._TWITCH_.PROCCESS.Normal
 
-import asyncio
+import asyncio, random
 
 class Quote(object):
 	async def Base(BASE, message, kwargs):
@@ -10,5 +10,29 @@ class Quote(object):
 
 		if channel_settings.get('active_quotes', False): return
 
-		channel_quotes = await BASE.modules._Twitch_.Utils.get_channel_quotes(BASE, message.channel_id)
-		BASE.modules.Console.DEBUG(channel_quotes) # TODO: make quotes quoting again
+		channel_quotes = kwargs.get('channel_quotes', None)
+		if channel_quotes == None:
+			channel_quotes = await BASE.modules._Twitch_.Utils.get_channel_quotes(BASE, message.channel_id)
+
+		if len(channel_quotes) == 0:
+			return await BASE.twitch.send_message(message.channel_name, 'This channel has no quotes.')
+
+		index = None
+		m = message.content.split(" ")
+		if len(m) > 1:
+			if m[1].isdigit():
+				index = int(m[1])
+
+		if index == None:
+			index = random.choice([q.get('id', None) for q in channel_quotes])
+
+		quote = None
+		for quo in channel_quotes:
+			if index == quo.get('id', None):
+				quote = quo.get('content', '[N/A]')
+				break
+
+		if quote == None:
+			return await BASE.twitch.send_message(message.channel_name, f"Quote '{str(index)}' was not found")
+
+		return await BASE.twitch.send_message(message.channel_name, quote + f" [ID: {str(index)}]")
