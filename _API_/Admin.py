8b171a92bf5cf2d088fab7ec4589092926c1a565
The_CJ <christoperjaekel@googlemail.com>
2018-12-09 17:07:27 +0100

    made new modules controll so removed old

--- _API_/Admin.py
+++ _API_/Admin.py
@@ -2,37 +2,6 @@
 
 import json, asyncio, time
 
-# TODO: renew
-def toggle_moduls(BASE, info={}, from_web=False, **kwargs):
-	"""toggle main Moduls status"""
-	session = info.get("cookies",{}).get("phaaze_session", None)
-	admin = BASE.api.utils.get_phaaze_user(BASE, session=session)
-
-	if admin.get('type', "").lower() == 'superadmin':
-		module = info['values'].get('modul',None)
-		if module == None: return
-
-		if eval("BASE.active.{} == True".format(module)):
-			setattr(BASE.active, module, False)
-			sta = 'False'
-
-		else:
-			setattr(BASE.active, module, True)
-			sta = 'True'
-
-		class r (object):
-			content = json.dumps(dict(status='success', msg='module `{}` now `{}`'.format(module, sta))).encode("UTF-8")
-			response = 200
-			header = [('Content-Type', 'application/json')]
-		return r
-
-	else:
-		class r (object):
-			content = json.dumps(dict(status='error', msg='unauthorised')).encode("UTF-8")
-			response = 400
-			header = [('Content-Type', 'application/json')]
-		return r
-
 # /api/admin/eval_command
 async def eval_command(self, request):
 	user_info = await self.root.get_user_info(request)
