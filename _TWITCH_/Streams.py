c1e04f7ab9d0a197b21e868783bcb5064e906c3a
the-cj <christoperjaekel@googlemail.com>
2018-12-23 03:55:07 +0100

    rewrote the main request loop

--- _TWITCH_/Streams.py
+++ _TWITCH_/Streams.py
@@ -4,12 +4,12 @@ import asyncio, discord, json
 
 #BASE.modules._Twitch_.Streams.Main
 class Init_Main(object):
-	""" Provides a """
+	""" Provides a information source for other modules, so there don't have to make a api request to twitch"""
 
 	def __init__(self, BASE):
 		super(Init_Main, self).__init__()
 		self.running = False
-		self.refresh_time = 30
+		self.refresh_time = 50
 
 		#add to BASE
 		self.BASE = BASE
@@ -27,7 +27,7 @@ class Init_Main(object):
 			# to reduce twitch requests as much as possible, we only update channels,
 			# that have at least one discord channel to alert or have Phaaze in the twitch channel
 
-			need_to_check = self.BASE.PhaazeDB.select(of="setting/twitch_channel", where="data['chat_managed'] or len(data['alert_discord_channel'])").get("data", [])
+			need_to_check = self.BASE.PhaazeDB.select(of="setting/twitch_channel", where="data['chat_managed'] or data['alert_discord_channel']").get("data", [])
 
 			try:
 				live_streams = self.BASE.modules._Twitch_.Utils.get_streams( self.BASE, [s['twitch_id'] for s in need_to_check if s.get('twitch_id', None) != None])
@@ -42,62 +42,51 @@ class Init_Main(object):
 				await asyncio.sleep(self.refresh_time * 0.75)
 				continue
 
-
-
-
-
 			id_list_of_live_channel = [ str(stream.get('channel', {}).get('_id', 0)) for stream in live_streams ]
-			old_ = self.generate_stream_obj(all_alerts, type_="db")
-			new_ = self.generate_stream_obj(live_streams, type_="twitch")
 
-			for twitch_id in old_:
+			old_list = self.generate_stream_list(need_to_check, source="db")
+			new_list = self.generate_stream_list(live_streams, source="twitch")
 
-				twitch_api_info = new_.get(twitch_id, {}).get('twitch_api_info', {})
-				db_info = old_.get(twitch_id, {}).get('db_info', {})
+			# every entry in new_list has a entry in old_list
+			# but not vise versa
+			# because the opposite in new_list is only there, if the channel is live
 
-				old_status = old_[twitch_id].get('live', False)
-				new_status = new_.get(twitch_id, {}).get('live', False)
+			for entry in old_list:
 
-				old_game = old_[twitch_id].get('game', None)
-				new_game = new_.get(twitch_id, {}).get('game', None)
+				old_stream = old_list.get(entry, {})
+				new_stream = new_list.get(entry, {}) # <- only there if live
+
+				twitch_info = new_stream.get('twitch_api_info', {})
+				db_info = old_stream.get('db_info', {})
+
+				old_status = old_stream.get('live', False)
+				new_status = new_stream.get('live', False)
+
+				old_game = old_stream.get('game', None)
+				new_game = new_stream.get('game', None)
 
 				# has gone live
 				if old_status != new_status and new_status == True:
 
-					self.event_live(twitch_info=twitch_api_info, db_info=db_info)
+					self.event_live(twitch_info=twitch_info, db_info=db_info)
 
 				# was live, changed game
 				elif old_game != new_game and new_game != None:
-					self.event_gamechange(twitch_info=twitch_api_info, db_info=db_info)
+					self.event_gamechange(twitch_info=twitch_info, db_info=db_info)
 
-			sorted_games_id_list = self.sort_game_channel_list(live_streams)
+			sorted_list_of_streams_per_game = self.sort_channel_per_game(live_streams)
 
-			self.BASE.PhaazeDB.update(of="twitch/alerts", content=f"data['live'] = True if data['twitch_id'] in {str(id_list_of_live_channel)} else False")
+			update_string = f"data['live'] = True if data['twitch_id'] in {str(id_list_of_live_channel)} else False;"
 
-			game_update = ""
-			for game in sorted_games_id_list:
-				id_list = sorted_games_id_list[game]
-				game_update += f"data['game'] = {json.dumps(game)} if data['twitch_id'] in {str(id_list)} else data.get('game', None);"
+			for game in sorted_list_of_streams_per_game:
+				id_channel_list = sorted_list_of_streams_per_game[game]
+				update_string += f"data['game'] = {json.dumps(game)} if data['twitch_id'] in {str(id_channel_list)} else data.get('game', None);"
 
-			self.BASE.PhaazeDB.update(of="twitch/alerts", content=game_update)
-			await asyncio.sleep(20)
+			self.BASE.PhaazeDB.update(of="twitch/stream", content=update_string)
+			await asyncio.sleep(self.refresh_time)
 
-	def get_alert_info(self, twitch_id, prevent_new=False):
-		check = self.BASE.PhaazeDB.select(of="twitch/alerts", where=f"data['twitch_id'] == '{twitch_id}'")
-		if check.get('hits', 0) < 1:
-			insert_ = dict()
-
-			insert_['twitch_id'] = twitch_id
-			insert_['live'] = False
-			insert_['game'] = None
-			insert_['discord_channel'] = []
-
-			if not prevent_new: self.BASE.PhaazeDB.insert(into="twitch/alerts", content=insert_)
-			return insert_
-
-		return check.get('data',[])[0]
-
-	def sort_game_channel_list(self, streams):
+	# utils
+	def sort_channel_per_game(self, streams):
 		r = {}
 
 		for st in streams:
@@ -112,8 +101,8 @@ class Init_Main(object):
 
 		return r
 
-	def generate_stream_obj(self, streams, type_="db"):
-		r = {}
+	def generate_stream_list(self, streams, source="db"):
+		r = dict()
 
 		for stream in streams:
 
@@ -124,20 +113,42 @@ class Init_Main(object):
 			twitch_api_info = None
 			db_info = None
 
-			if type_ == "db":
+			if source == "db":
 				key = stream.get('twitch_id', None)
 				game = stream.get('game', None)
 				live = stream.get('live', False)
 				db_info = stream
-			elif type_ == "twitch":
+
+			elif source == "twitch":
 				key = stream.get('channel', {}).get('_id', None)
 				game = stream.get('game', None)
 				live = True
 				twitch_api_info = stream
 
-			r[str(key)] = dict( game=game, live=live, twitch_api_info=twitch_api_info, db_info=db_info )
+			r[str(key)] = dict(
+				game=game,
+				live=live,
+				twitch_api_info=twitch_api_info,
+				db_info=db_info
+			)
 
 		return r
+
+	#functions
+	def get_alert_info(self, twitch_id, prevent_new=False):
+		check = self.BASE.PhaazeDB.select(of="twitch/alerts", where=f"data['twitch_id'] == '{twitch_id}'")
+		if check.get('hits', 0) < 1:
+			insert_ = dict()
+
+			insert_['twitch_id'] = twitch_id
+			insert_['live'] = False
+			insert_['game'] = None
+			insert_['discord_channel'] = []
+
+			if not prevent_new: self.BASE.PhaazeDB.insert(into="twitch/alerts", content=insert_)
+			return insert_
+
+		return check.get('data',[])[0]
 
 	#Stream Events
 	def event_live(self, twitch_info=dict(), db_info=dict()):
