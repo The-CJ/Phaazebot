f667e51e2d6e5a27c1df515d23879dd0e23301b7
the-cj <christoperjaekel@googlemail.com>
2019-01-15 20:43:48 +0100

    first changes to /account

---
+++ _WEB_/js/site-specific/account/main.js
@@ -0,0 +1,9 @@
+function show_newpassword() {
+  $("#pw_btn").hide();
+  $("#new_password").closest(".col").show();
+  $("#new_password2").closest(".col").show();
+}
+
+function save_changes() {
+  
+}
