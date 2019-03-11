0662e06f9e146eaf2b32ec7f019a5193114dcab1
the-cj <christoperjaekel@googlemail.com>
2019-03-11 17:04:10 +0100

    refactored discord html invite

--- _WEB_/processing/discord/invite.py
+++ _WEB_/processing/discord/invite.py
@@ -1,26 +1,25 @@
-#BASE.modules._Web_.Base.root.discord.invite
+import asyncio
 
-def invite(BASE, info, root, dump, msg="", server_id=""):
-	return_header = [('Content-Type','text/html')]
+# /discord/invite
+async def main(self, request):
+	user_info = await self.root.get_user_info(request)
 
-	site = open("_WEB_\content\discord\discord_invite.html", 'r').read()
-
-	site = site.replace("<!-- message -->", msg)
-
-	if server_id != "":
-		invite_link = "https://discordapp.com/oauth2/authorize?client_id=180679855422177280&scope=bot&permissions=8&guild_id="+server_id
-	else:
-		invite_link = "https://discordapp.com/oauth2/authorize?client_id=180679855422177280&scope=bot&permissions=8"
+	main_site = open("_WEB_\content\discord\discord_invite.html", 'r').read()
+	current_navbar = self.root.html_header(self.root.BASE, user_info = user_info, active="admin")
 
 	site = site.replace("<!-- server_invite -->", invite_link)
 
-	site = BASE.modules._Web_.Utils.format_html_functions(BASE, site, infos = info)
-	class r (object):
-		content = site.encode("UTF-8")
-		response = 200
-		header = return_header
-	return r
+	site = self.root.format_html(self.root.html_root,
+		title="Phaaze | Discord - Invite",
+		header=current_navbar,
+		main=main_site
+	)
 
+	return self.root.response(
+		body=site,
+		status=200,
+		content_type='text/html'
+	)
 
 
 
