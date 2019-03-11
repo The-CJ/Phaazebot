75182378e054528759ff8fd9469e9ae823352f0a
the-cj <christoperjaekel@googlemail.com>
2019-03-11 17:24:52 +0100

    refactor discord invite build up

--- _WEB_/processing/discord/invite.py
+++ _WEB_/processing/discord/invite.py
@@ -1,13 +1,24 @@
 import asyncio
 
+DISCORD_BOT_CLIENT_ID = "180679855422177280"
+
 # /discord/invite
-async def main(self, request):
+async def main(self, request, msg="", server_id=None):
 	user_info = await self.root.get_user_info(request)
 
-	main_site = open("_WEB_\content\discord\discord_invite.html", 'r').read()
-	current_navbar = self.root.html_header(self.root.BASE, user_info = user_info, active="admin")
+	main_site = open("_WEB_/content/discord/discord_invite.html", 'r').read()
+	current_navbar = self.root.html_header(self.root.BASE, user_info = user_info, active="discord")
 
-	site = site.replace("<!-- server_invite -->", invite_link)
+	invite_link = f"https://discordapp.com/oauth2/authorize?client_id={DISCORD_BOT_CLIENT_ID}&scope=bot&permissions=8"
+
+	check_id = server_id or request.query.get("server_id", None)
+	if check_id != None:
+		invite_link += f"&guild_id={check_id}"
+
+	main_site = self.root.format_html(main_site,
+		message=msg,
+		invite_link=invite_link
+	)
 
 	site = self.root.format_html(self.root.html_root,
 		title="Phaaze | Discord - Invite",
