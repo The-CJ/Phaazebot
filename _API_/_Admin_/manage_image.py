37cdb87a83220007f88bf30d8858d939a3422c2d
the-cj <christoperjaekel@googlemail.com>
2019-03-17 20:19:59 +0100

    added file upload with all checks

--- _API_/_Admin_/manage_image.py
+++ _API_/_Admin_/manage_image.py
@@ -1,4 +1,5 @@
-import asyncio, json, os
+import asyncio, json, os, io
+from PIL import Image
 
 # /api/admin/manage-image
 async def main(self, request):
@@ -60,9 +61,46 @@ async def upload(self, request):
 	if not self.root.check_role(user_info, ['image uploader','admin']):
 		return await self.action_not_allowed(request, msg="Insufficient rights.")
 
+	image_name = None
+	image_file = None
+
+	rm = await request.multipart()
+	while not rm.at_eof():
+		part = await rm.next()
+		if part == None: continue
+
+		if part.name == "name": image_name = await part.text()
+		if part.name == "file": image_file = await part.read()
+
+	if image_name == None or image_file == None:
+		return self.root.response(
+			body=json.dumps(dict(status=400, msg="missing field 'name' or 'file'")),
+			status=400,
+			content_type='application/json'
+		)
+
+	image_name = str(image_name).replace("..", "").replace("/", "").replace(" ", "_")
+
+	if image_name in [None, ""]:
+		return self.root.response(
+			body=json.dumps(dict(status=400, msg="field 'name' is invalid")),
+			status=400,
+			content_type='application/json'
+		)
+
+	try:
+		image_file = Image.open(io.BytesIO(image_file))
+		image_file.save(f"{self.root.BASE.vars.IMAGE_PATH}{image_name}")
+	except:
+		return self.root.response(
+			body=json.dumps(dict(status=400, msg="file is not a valid image file")),
+			status=400,
+			content_type='application/json'
+		)
+
 	return self.root.response(
-		body=json.dumps(dict(status=204)),
-		status=204,
+		body=json.dumps(dict(status=200, msg=f"successfull saved as: '{image_name}'")),
+		status=200,
 		content_type='application/json'
 	)
 
