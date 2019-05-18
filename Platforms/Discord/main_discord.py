0d9b93dd41df6c12d66907fba776ea9fce7d74e6
The-CJ <christoperjaekel@googlemail.com>
2019-05-18 21:18:02 +0200

    base construct for discord

---
+++ Platforms/Discord/main_discord.py
@@ -0,0 +1,54 @@
+#import asyncio
+import discord
+#import traceback
+
+class PhaazebotDiscord(discord.Client):
+	def __init__(self, BASE):
+		self.BASE = BASE
+		super().__init__()
+
+	async def on_ready(self):
+		pass
+
+	async def on_message(self, message):
+		pass
+
+	async def on_message_delete(self, message):
+		pass
+
+	async def on_message_edit(self, before, after):
+		pass
+
+	#member management
+	async def on_member_join(self, member):
+		pass
+
+	async def on_member_remove(self, member):
+		pass
+
+	async def on_member_ban(self, member):
+		pass
+
+	async def on_member_unban(self, server, user):
+		pass
+
+	async def on_member_update(self, before, after):
+		pass
+
+	#channel management
+	async def on_channel_create(self, channel):
+		pass
+
+	async def on_channel_delete(self, channel):
+		pass
+
+	#role management
+	async def on_server_role_create(self, role):
+		pass
+
+	async def on_server_role_delete(self, role):
+		pass
+
+	#errors
+	async def on_error(self, event_method, *args, **kwargs):
+		pass
