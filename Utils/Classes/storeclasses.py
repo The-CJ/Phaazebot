b96a34d8873910ea3ed41cfc3c7cf3e92351adca
The-CJ <christoperjaekel@googlemail.com>
2019-05-18 23:16:08 +0200

    updated example and strict type convertion in stores

--- Utils/Classes/storeclasses.py
+++ Utils/Classes/storeclasses.py
@@ -5,25 +5,25 @@ class AccessStore(object):
 		Settings and Keys to all platforms or entry points that is not us
 	"""
 	def __init__(self, config:ConfigParser):
-		self.twitch_api_token:str = config.get('twitch_api_token', '')
-		self.twitch_irc_token:str = config.get('twitch_irc_token', '')
-		self.twitch_admin_token:str = config.get('twitch_admin_token', '')
+		self.twitch_api_token:str = str(config.get('twitch_api_token', ''))
+		self.twitch_irc_token:str = str(config.get('twitch_irc_token', ''))
+		self.twitch_admin_token:str = str(config.get('twitch_admin_token', ''))
 
-		self.discord_token:str = config.get('discord_token', '')
+		self.discord_token:str = str(config.get('discord_token', ''))
 
-		self.osu_api_token:str = config.get('osu_api_token', '')
-		self.osu_irc_token:str = config.get('osu_irc_token', '')
+		self.osu_api_token:str = str(config.get('osu_api_token', ''))
+		self.osu_irc_token:str = str(config.get('osu_irc_token', ''))
 
-		self.cleverbot_token:str = config.get('cleverbot_token', '')
+		self.cleverbot_token:str = str(config.get('cleverbot_token', ''))
 
-		self.mashape_token:str = config.get('mashape_token', '')
+		self.mashape_token:str = str(config.get('mashape_token', ''))
 
-		self.phaazedb_token:str = config.get('phaazedb_token', '')
+		self.phaazedb_token:str = str(config.get('phaazedb_token', ''))
 
-		self.twitter_token:str = config.get('twitter_token','')
-		self.twitter_token_key:str = config.get('twitter_token_key','')
-		self.twitter_consumer_key:str = config.get('twitter_consumer_key','')
-		self.twitter_consumer_secret:str = config.get('twitter_consumer_secret','')
+		self.twitter_token:str = str(config.get('twitter_token',''))
+		self.twitter_token_key:str = str(config.get('twitter_token_key',''))
+		self.twitter_consumer_key:str = str(config.get('twitter_consumer_key',''))
+		self.twitter_consumer_secret:str = str(config.get('twitter_consumer_secret',''))
 
 class ActiveStore(object):
 	"""
@@ -51,37 +51,37 @@ class LimitStore(object):
 
 	"""
 	def __init__(self, config:ConfigParser):
-		self.DISCORD_PRIVATE_COOLDOWN:int = config.get("discord_private_cooldown", 1)
-		self.DISCORD_NORMAL_COOLDOWN:int = config.get("discord_normal_cooldown", 1)
-		self.DISCORD_MOD_COOLDOWN:int = config.get("discord_mod_cooldown", 3)
-		self.DISCORD_OWNER_COOLDOWN:int = config.get("discord_owner_cooldown", 5)
-		self.DISCORD_CUSTOM_COMAMNDS_AMOUNT:int = config.get("discord_custom_commands_amount", 100)
-		self.DISCORD_CUSTOM_COMMANDS_COOLDOWN:int = config.get("discord_custom_commands_cooldown", 3)
-		self.DISCORD_LEVEL_COOLDOWN:int = config.get("discord_level_cooldown", 3)
-		self.DISCORD_QUOTES_AMOUNT:int = config.get("discord_quotes_amount", 100)
-		self.DISCORD_ADDROLE_AMOUNT:int = config.get("discord_addrole_amount", 25)
+		self.DISCORD_PRIVATE_COOLDOWN:int = int(config.get("discord_private_cooldown", 1))
+		self.DISCORD_NORMAL_COOLDOWN:int = int(config.get("discord_normal_cooldown", 1))
+		self.DISCORD_MOD_COOLDOWN:int = int(config.get("discord_mod_cooldown", 3))
+		self.DISCORD_OWNER_COOLDOWN:int = int(config.get("discord_owner_cooldown", 5))
+		self.DISCORD_CUSTOM_COMAMNDS_AMOUNT:int = int(config.get("discord_custom_commands_amount", 100))
+		self.DISCORD_CUSTOM_COMMANDS_COOLDOWN:int = int(config.get("discord_custom_commands_cooldown", 3))
+		self.DISCORD_LEVEL_COOLDOWN:int = int(config.get("discord_level_cooldown", 3))
+		self.DISCORD_QUOTES_AMOUNT:int = int(config.get("discord_quotes_amount", 100))
+		self.DISCORD_ADDROLE_AMOUNT:int = int(config.get("discord_addrole_amount", 25))
 
-		self.TWITCH_TIMEOUT_MESSAGE_COOLDOWN:int = config.get("twitch_timeout_message_cooldown", 20)
-		self.TWITCH_BLACKLIST_REMEMBER_TIME:int = config.get("twitch_blacklist_remember_time", 180)
-		self.TWITCH_CUSTOM_COMMAND_AMOUNT:int = config.get("twitch_custom_command_amount", 100)
-		self.TWITCH_QUOTE_AMOUNT:int = config.get("twitch_quote_amount", 100)
-		self.TWITCH_STATS_COOLDOWN:int = config.get("twitch_stats_cooldown", 5)
+		self.TWITCH_TIMEOUT_MESSAGE_COOLDOWN:int = int(config.get("twitch_timeout_message_cooldown", 20))
+		self.TWITCH_BLACKLIST_REMEMBER_TIME:int = int(config.get("twitch_blacklist_remember_time", 180))
+		self.TWITCH_CUSTOM_COMMAND_AMOUNT:int = int(config.get("twitch_custom_command_amount", 100))
+		self.TWITCH_QUOTE_AMOUNT:int = int(config.get("twitch_quote_amount", 100))
+		self.TWITCH_STATS_COOLDOWN:int = int(config.get("twitch_stats_cooldown", 5))
 
-		self.WEB_CLIENT_MAX_SIZE:int = config.get("web_client_max_size", 5242880) #5MB
+		self.WEB_CLIENT_MAX_SIZE:int = int(config.get("web_client_max_size", 5242880)) #5MB
 
 class VarsStore(object):
 	"""
 		filled with permanent vars, or functions to get vars
 	"""
 	def __init__(self, config:ConfigParser):
-		self.trigger_discord:str = config.get('trigger_discord', '>')
-		self.trigger_osu:str = config.get('trigger_osu', '!')
-		self.trigger_twitch:str = config.get('trigger_twitch', '!')
+		self.trigger_discord:str = str(config.get('trigger_discord', '>'))
+		self.trigger_osu:str = str(config.get('trigger_osu', '!'))
+		self.trigger_twitch:str = str(config.get('trigger_twitch', '!'))
 
-		self.default_twitch_currency:str = config.get('default_twitch_currency', 'Credit')
-		self.default_twitch_currency_multi:str = config.get('default_twitch_currency_multi', 'Credits')
+		self.default_twitch_currency:str = str(config.get('default_twitch_currency', 'Credit'))
+		self.default_twitch_currency_multi:str = str(config.get('default_twitch_currency_multi', 'Credits'))
 
-		self.image_path:str = config.get('image_path', '_WEB_/img/')
+		self.image_path:str = str(config.get('image_path', '_WEB_/img/'))
 
 		self.logo_osu:str = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Osu%21Logo_%282015%29.png/600px-Osu%21Logo_%282015%29.png"
 		self.logo_twitch:str = "https://i.redditmedia.com/za3YAsq33WcZc66FVb1cBw6mY5EibKpD_5hfLz0AbaE.jpg?w=320&s=53cf0ff252d84c5bb460b6ec0b195504"
