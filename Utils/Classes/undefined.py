379ca80ca1cad48d2ab77c70310f43c08be8cd00
The-CJ <christoperjaekel@googlemail.com>
2019-05-18 20:17:02 +0200

    ignore self

--- Utils/Classes/undefined.py
+++ Utils/Classes/undefined.py
@@ -4,8 +4,12 @@ class Undefined():
 	"""
 	def __init__(self): pass
 
-	def __eq__(self, value): return False # ==
-	def __ne__(self, value): return False # !=
+	def __eq__(self, value): # ==
+		if type(value) == Undefined: return True
+		return False
+	def __ne__(self, value): # !=
+		if type(value) != Undefined: return True
+		return False
 
 	def __ge__(self, value): return False # >=
 	def __gt__(self, value): return False # >
@@ -13,4 +17,4 @@ class Undefined():
 	def __le__(self, value): return False # <=
 	def __lt__(self, value): return False # <
 
-	def __bool__(self): return False # if 
+	def __bool__(self): return False # if
