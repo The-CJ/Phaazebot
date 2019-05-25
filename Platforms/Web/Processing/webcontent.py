9dd9a934af50fe978461502e41263ff5a76f436e
the-cj <christoperjaekel@googlemail.com>
2019-05-25 19:14:58 +0200

    actully serving of the functions

--- Platforms/Web/Processing/webcontent.py
+++ Platforms/Web/Processing/webcontent.py
@@ -1,3 +1,57 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Web.index import WebIndex
+
+import json, mimetypes
+from aiohttp.web import Request, Response
+
+CONTENTFOLDER_CSS = "Platforms/Web/Content/Css"
+CONTENTFOLDER_JS = "Platforms/Web/Content/Js"
+CONTENTFOLDER_IMG = "Platforms/Web/Content/Img"
+
+# content serve functions
+async def serveCss(self:"WebIndex", Request:Request) -> Response:
+	file_location:str = Request.match_info.get("file", None)
+	if not file_location: return noFileDefined(self)
+
+	file_location = file_location.replace("..","").strip("/")
+
+	file_content:bytes = open(f"{CONTENTFOLDER_CSS}/{file_location}", "rb").read()
+
+	return self.response(
+		status=200,
+		content_type='text/css',
+		body=file_content
+	)
+
+async def serveJs(self:"WebIndex", Request:Request) -> Response:
+	file_location:str = Request.match_info.get("file", None)
+	if not file_location: return noFileDefined(self)
+
+	file_location = file_location.replace("..","").strip("/")
+
+	file_content:bytes = open(f"{CONTENTFOLDER_JS}/{file_location}", "rb").read()
+
+	return self.response(
+		status=200,
+		content_type='application/json',
+		body=file_content
+	)
+
+async def serveImg(self:"WebIndex", Request:Request) -> Response:
+	file_location:str = Request.match_info.get("file", None)
+	if not file_location: return noFileDefined(self)
+
+	file_location = file_location.replace("..","").strip("/")
+
+	file_content:bytes = open(f"{CONTENTFOLDER_IMG}/{file_location}", "rb").read()
+
+	return self.response(
+		status=200,
+		content_type=mimetypes.guess_type(f"{CONTENTFOLDER_IMG}/{file_location}", strict=True)[0],
+		body=file_content
+	)
+
 # error handling
 async def noFileDefined(cls:"WebIndex") -> Response:
 	return cls.response(
