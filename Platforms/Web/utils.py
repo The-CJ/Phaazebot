d6438d6027187567ab84baf431119ea59a6be454
the-cj <christoperjaekel@googlemail.com>
2019-05-26 03:18:29 +0200

    switched to AF

--- Platforms/Web/utils.py
+++ Platforms/Web/utils.py
@@ -1,11 +1,14 @@
-# from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
-# 	from Platforms.Web.index import WebIndex
-
-# import html
-# from aiohttp.web import Request, Response
 from Utils.Classes.htmlformatter import HTMLFormatter
 
-def getNavbar(style:str="", user_info=None) -> HTMLFormatter:
+def getNavbar(active:str="", user_info=None) -> HTMLFormatter:
 	navbar:HTMLFormatter = HTMLFormatter("Platforms/Web/Content/Html/Navbar/default.html")
+
+
+
+	navbar.setRegex(r"\{selected_(.+?)\}")
+	navbar.replace(replace_empty=True, **{active:"active"})
+
 	return navbar
+
+def getButton() -> HTMLFormatter:
+	pass
