27d34d9889e2fbef122c28ed90486c5c3500443a
the-cj <christoperjaekel@googlemail.com>
2019-05-26 03:46:40 +0200

    staged out link , so bots have it harder

---
+++ Platforms/Web/Content/Js/master.js
@@ -0,0 +1,8 @@
+function showEmail() {
+  // i do this, so spam bots don't get the email from the site so easy
+  $("#email_icon").popover({
+    content: ["admin","@", "pha", "aze", ".", "net"].join(""),
+    placement:"bottom",
+    trigger:"hover"
+  }).popover()
+}
