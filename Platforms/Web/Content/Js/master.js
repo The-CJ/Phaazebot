457a1db304c55fb375472e2d281eafb341752b62
the-cj <christoperjaekel@googlemail.com>
2019-06-02 02:38:20 +0200

    show wrong login data

--- Platforms/Web/Content/Js/master.js
+++ Platforms/Web/Content/Js/master.js
@@ -35,15 +35,45 @@ var SessionManager = new (class SessionManager {
     let password = $("#phaaze_password").val();
     $.post("/api/account/phaaze/login", {"phaaze_username":user, "phaaze_password":password})
     .done(function (data) {
-      console.log(data);
+      CookieManager.set("phaaze_session", data.phaaze_session);
     })
     .fail(function (data) {
-      console.log(data);
+      $("#phaaze_email_or_username").addClass("animated shake");
+      $("#phaaze_password").addClass("animated shake").val("");
+      setTimeout(function () {
+        $("#phaaze_password, #phaaze_email_or_username").removeClass("animated shake");
+      }, 1000);
     })
-
   }
   logout(field) {
 
+  }
+})()
+
+var CookieManager = new (class CookieManager {
+  constructor() {
+
+  }
+  get(cookie) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
+  set(name, value) {
+    document.cookie = name+"="+value+'; Path=/';
+  }
+  remove(name) {
+    document.cookie = name+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=\"/\""
   }
 })()
 
