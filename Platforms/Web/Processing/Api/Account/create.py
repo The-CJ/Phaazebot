70790204f1fd6036409af6b5060b41f65fbf06b3
the-cj <christoperjaekel@googlemail.com>
2019-06-20 16:11:58 +0200

    more account create

--- Platforms/Web/Processing/Api/Account/create.py
+++ Platforms/Web/Processing/Api/Account/create.py
@@ -7,7 +7,9 @@ import re
 from aiohttp.web import Response, Request
 from Utils.Classes.webuserinfo import WebUserInfo
 from Utils.Classes.webrequestcontent import WebRequestContent
+from Platforms.Web.utils import searchUser
 from Utils.regex import IsEmail
+from Utils.stringutils import password as password_function
 
 async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response:
 	"""
@@ -17,7 +19,7 @@ async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response
 	UserInfo:WebUserInfo = await cls.getUserInfo(WebRequest)
 
 	if UserInfo.found:
-		cls.Web.BASE.Logger.debug(f"Account create already exists - User ID: {UserInfo.user_id}", require="api:create")
+		cls.Web.BASE.Logger.debug(f"(API) Account create already exists - User ID: {UserInfo.user_id}", require="api:create")
 		return cls.response(
 			body=json.dumps( dict(error="aleady_logged_in", status=400, msg="no registion needed, already logged in") ),
 			content_type="application/json",
@@ -33,6 +35,7 @@ async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response
 	password2:str = Data.get("password2")
 
 	if password != password2:
+		cls.Web.BASE.Logger.debug(f"(API) Account create failed, passwords don't match", require="api:create")
 		return cls.response(
 			body=json.dumps( dict(error="unequal_passwords", status=400, msg="the passwords are not the same") ),
 			content_type="application/json",
@@ -40,19 +43,41 @@ async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response
 		)
 
 	if len(password) < 7:
+		cls.Web.BASE.Logger.debug(f"(API) Account create failed, password to short", require="api:create")
 		return cls.response(
 			body=json.dumps( dict(error="invalid_password", status=400, msg="the password must be at least 8 chars long") ),
 			content_type="application/json",
 			status=400
 		)
 
-	if re.match(IsEmail, email):
+	if not re.match(IsEmail, email):
+		cls.Web.BASE.Logger.debug(f"(API) Account create failed, email looks false: {str(email)}", require="api:create")
 		return cls.response(
 			body=json.dumps( dict(error="invalid_email", status=400, msg="email looks false") ),
 			content_type="application/json",
 			status=400
 		)
 
+	check:list = await searchUser(cls, f"user['username'].lower() == {json.dumps(username)}.lower() or user['email'].lower() == {json.dumps(email)}.lower()")
+	if check:
+		cls.Web.BASE.Logger.debug(f"(API) Account create failed, account already taken: {str(username)} - {str(email)}", require="api:create")
+		return cls.response(
+			body=json.dumps( dict(error="account_taken", status=400, msg="username or email is already taken") ),
+			content_type="application/json",
+			status=400
+		)
+
+	# everything ok, create
+
+	new_user:dict = dict(
+		username= username,
+		password = password_function(password),
+		email = email,
+	)
+
+	#TODO: need to send email verification
+
+	cls.Web.BASE.Logger.debug(f"(API) Account created: {str(new_user)}", require="api:create")
 	return cls.response(
 		text=json.dumps( dict(debug=Data.content,status=200) ),
 		content_type="application/json",
