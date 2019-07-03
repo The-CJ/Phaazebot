61e6bf840856d71e883479ef6c2c687cb606ab2c
the-cj <christoperjaekel@googlemail.com>
2019-07-03 17:35:36 +0200

    page load

--- Platforms/Web/Processing/Account/accountmain.py
+++ Platforms/Web/Processing/Account/accountmain.py
@@ -3,6 +3,7 @@ if TYPE_CHECKING:
 	from Platforms.Web.index import WebIndex
 
 from aiohttp.web import Response, Request
+from Utils.Classes.htmlformatter import HTMLFormatter
 from Utils.Classes.webuserinfo import WebUserInfo
 from Platforms.Web.utils import getNavbar
 
@@ -11,14 +12,16 @@ async def accountMain(cls:"WebIndex", WebRequest:Request) -> Response:
 		Default url: /account
 	"""
 	UserInfo:WebUserInfo = await cls.getUserInfo(WebRequest)
-
 	if not UserInfo.found: return await cls.accountLogin(WebRequest)
+
+	AccountPage:HTMLFormatter = HTMLFormatter("Platforms/Web/Content/Html/Account/main.html")
 
 	site:str = cls.HTMLRoot.replace(
 		replace_empty = True,
 
 		title = "Phaaze | Account",
-		header = getNavbar()
+		header = getNavbar(),
+		main = AccountPage
 	)
 
 	return cls.response(
