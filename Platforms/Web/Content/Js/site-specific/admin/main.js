43e163f8b02cf93546216a2b3c3b5c77042c2438
the-cj <christoperjaekel@googlemail.com>
2019-07-07 23:59:53 +0200

    prepare for admin eval

--- Platforms/Web/Content/Js/site-specific/admin/main.js
+++ Platforms/Web/Content/Js/site-specific/admin/main.js
@@ -1,3 +1,16 @@
-function evelCommand() {
-  console.log("e");
+function evelCommand(command) {
+  if (command == null) {
+    command = $('#eval_command').val();
+  }
+  var r = {
+    command: command
+  };
+  $.post("/api/admin/module/evaluate", r)
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (data) {
+      let msg = data.responseJSON ? data.responseJSON.error : "unknown";
+      Display.showMessage( {content:msg, color:Display.color_critical} );
+    })
 }
