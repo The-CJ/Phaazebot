ecdb170f3d45e3a11abb56bc0b9e29fcf45f793f
the-cj <christoperjaekel@googlemail.com>
2019-07-07 19:46:26 +0200

    load all main infos for user

--- Platforms/Web/Content/Js/site-specific/account/main.js
+++ Platforms/Web/Content/Js/site-specific/account/main.js
@@ -2,3 +2,19 @@ function showPasswordFields() {
   $("[new_password_space] > button").hide();
   $("[new_password_space] > div").show();
 }
+
+function getAccountInfos() {
+  $.get("/api/account/phaaze/get")
+  .done(function (data) {
+    insertData("main", data.user);
+  })
+  .fail(function (data) {
+    console.log(data);
+    let msg = "Could not load account infomations";
+    Display.showMessage({ content: msg, color: Display.color_critical });
+  })
+}
+
+$("document").ready(
+  getAccountInfos
+)
