c79d1a07c0aa96dbd3e0412ed73deef7e3c2a1e9
the-cj <christoperjaekel@googlemail.com>
2019-07-07 20:03:38 +0200

    edit in alternative account now posssibe

--- Platforms/Web/Content/Js/site-specific/account/main.js
+++ Platforms/Web/Content/Js/site-specific/account/main.js
@@ -3,11 +3,28 @@ function showPasswordFields() {
   $("[new_password_space] > div").show();
 }
 
+function saveAccountInfos() {
+  var data = extractData("#manage_field");
+  var real_data = {};
+  for (var d in data) {
+    real_data["phaaze_"+d] = data[d];
+  }
+  $.post("/api/account/phaaze/edit", real_data)
+  .done(function (data) {
+    Display.showMessage({content:data.msg, color:Display.color_success});
+    getAccountInfos();
+  })
+  .fail(function (data) {
+    let msg = data.responseJSON ? data.responseJSON.msg : "unknown"
+    Display.showMessage({content:msg, color:Display.color_critical});
+  })
+}
+
 function getAccountInfos() {
   $.get("/api/account/phaaze/get")
   .done(function (data) {
     insertData("#manage_field", data.user);
-    var role_field = $("#role_field");
+    var role_field = $("#role_field").html("");
     for (role of data.user.roles) {
       let r = $("<div class='role'>");
       r.text(role);
