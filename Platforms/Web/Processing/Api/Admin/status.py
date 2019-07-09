24735ce8945d206b770f305fdc25f7c4193b45f7
the-cj <christoperjaekel@googlemail.com>
2019-07-09 20:13:22 +0200

    added discord to status api

--- Platforms/Web/Processing/Api/Admin/status.py
+++ Platforms/Web/Processing/Api/Admin/status.py
@@ -28,8 +28,34 @@ async def apiAdminStatus(cls:"WebIndex", WebRequest:Request, Data:WebRequestCont
 	for module in vars(BASE.Active):
 		status["modules"][module] = bool(getattr(BASE.Active, module, False))
 
+	if BASE.Active.discord and BASE.IsReady.discord:
+		status["discord"] = getDiscordStatus(BASE)
+	else:
+		status["discord"] = None
+
 	return cls.response(
 		body=json.dumps(dict(result=status, status=200)),
 		status=200,
 		content_type='application/json'
 	)
+
+def getDiscordStatus(BASE:"Phaazebot") -> dict:
+	discord:dict = dict(
+		unique_guilds = len(BASE.Discord.guilds),
+		unique_users = getUniqueDiscordMember(BASE.Discord.guilds),
+		bot_id = BASE.Discord.user.id,
+		bot_name = BASE.Discord.user.name,
+		bot_discriminator = BASE.Discord.user.discriminator,
+		bot_avatar_url = BASE.Discord.user.avatar_url,
+	)
+
+	return discord
+
+def getUniqueDiscordMember(guilds:list) -> int:
+	unique_member:list = []
+
+	for guild in guilds:
+		for member in guild.members:
+			if member.id not in unique_member: unique_member.append(member.id)
+
+	return len(unique_member)
