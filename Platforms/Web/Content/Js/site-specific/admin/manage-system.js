0d37a9c0145667015995c96edfc0e68ef798d4a5
the-cj <christoperjaekel@googlemail.com>
2019-07-09 19:30:03 +0200

    build status time and modules

--- Platforms/Web/Content/Js/site-specific/admin/manage-system.js
+++ Platforms/Web/Content/Js/site-specific/admin/manage-system.js
@@ -1,3 +1,35 @@
 function loadStatus() {
-
+  $.get("/api/admin/status")
+  .done(function (data) {
+    $("#version").text(data.result.version);
+    buildUptime(data.result.uptime);
+    buildModules(data.result.modules)
+  })
+  .fail(function (data) {
+    console.log(data);
+  })
 }
+
+function buildUptime(seconds) {
+  // uptime
+  let minutes = seconds / 60;
+  let hours = minutes / 60
+  seconds = seconds % 60
+  minutes = minutes % 60
+  $("#uptime").text(parseInt(hours)+"h "+parseInt(minutes)+"m "+parseInt(seconds)+"s");
+}
+
+function buildModules(modules) {
+  var Target = $("[part=phaaze] [modules]");
+  for (m in modules) {
+    var Field = $("[phantom] > .module").clone();
+    Field.find(".name").text(m);
+    Field.find(".value").text(modules[m] ? "Enabled" : "Disabled");
+    Field.find(".value").attr("active", modules[m] ? "true" : "false");
+    Target.append(Field);
+  }
+}
+
+$("document").ready(function () {
+  loadStatus();
+})
