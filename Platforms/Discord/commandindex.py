a8fd16015486c62590c5a9a2a412361d6850c244
the-cj <christoperjaekel@googlemail.com>
2019-07-13 23:27:11 +0200

    for me to note stuff

--- Platforms/Discord/commandindex.py
+++ Platforms/Discord/commandindex.py
@@ -2,20 +2,20 @@ from typing import Awaitable
 
 from .Processing.Simple.register import  simple_register
 from .Processing.Complex.register import complex_register
-from .Processing.Simple.textonly import textOnly
+from .Processing.Simple.textonly import textOnly as standardCommandFunction
 
 def getDiscordCommandFunction(function_type:str, command_name:str) -> Awaitable:
 	"""
 		get the associated function to name, else hanbdle it a text only
 	"""
 	# should not happen
-	if not command_name: return textOnly
+	if not command_name: return standardCommandFunction
 
 	if function_type == "simple":
-		return simple_register.get(command_name, textOnly)
+		return simple_register.get(command_name, standardCommandFunction)
 
 	elif function_type == "complex":
-		return complex_register.get(command_name, textOnly)
+		return complex_register.get(command_name, standardCommandFunction)
 
 	else:
-		return textOnly
+		return standardCommandFunction
