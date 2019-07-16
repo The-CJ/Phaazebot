777fedd50f513c4686329c8276caeee2b50bd6b7
the-cj <christoperjaekel@googlemail.com>
2019-07-17 00:07:02 +0200

    added api to discord end

---
+++ Platforms/Discord/api.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from main import Phaazebot
+
+import requests
+from aiohttp.web import Request
+
+ROOT_URL = "https://discordapp.com/api/v6/"
+
+async def translateDiscordToken(cls:"Phaazebot", WebRequest:Request) -> dict or None:
+	code:str = WebRequest.query.get("code", None)
+	if not code:
+		cls.Logger.debug("translateDiscordToken called without code", require="discord:api")
+		return None
+
+	req:dict = dict(
+		client_id = cls.Vars.DISCORD_BOT_ID,
+		client_secret = cls.Access.DISCORD_SECRET,
+		grant_type = "authorization_code",
+		code = code,
+		redirect_uri = cls.Vars.DISCORD_REDIRECT_LINK
+	)
+	headers:dict = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+	res = requests.post(ROOT_URL+"oauth2/token", req, headers)
+	return res.json()
--- Platforms/Web/Processing/Discord/discordlogin.py
+++ Platforms/Web/Processing/Discord/discordlogin.py
@@ -6,13 +6,20 @@ from aiohttp.web import Response, Request
 from Utils.Classes.htmlformatter import HTMLFormatter
 from Utils.Classes.discorduserinfo import DiscordUserInfo
 from Platforms.Web.utils import getNavbar
+from Platforms.Discord.api import translateDiscordToken
 
 async def discordLogin(cls:"WebIndex", WebRequest:Request, msg:str="") -> Response:
 	"""
 		Default url: /discord/login
 	"""
+	print(WebRequest.raw_path)
 	DiscordUser:DiscordUserInfo = await cls.getDiscordUserInfo(WebRequest)
 	if DiscordUser.found: return await cls.discordMain(WebRequest)
+
+	# use has a ?code=something -> check login
+	if WebRequest.query.get("code", False):
+		f = await translateDiscordToken(cls.Web.BASE, WebRequest)
+		print(f)
 
 	DiscordLogin:HTMLFormatter = HTMLFormatter("Platforms/Web/Content/Html/Discord/login.html")
 	DiscordLogin.replace(
@@ -21,7 +28,6 @@ async def discordLogin(cls:"WebIndex", WebRequest:Request, msg:str="") -> Respon
 		msg = msg,
 		login_link = cls.Web.BASE.Vars.DISCORD_LOGIN_LINK
 	)
-
 
 	site:str = cls.HTMLRoot.replace(
 		replace_empty = True,
