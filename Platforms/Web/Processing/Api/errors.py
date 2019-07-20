cdd6e4a13358b05b5c2ed5cc9bbe4c7ae643d427
the-cj <christoperjaekel@googlemail.com>
2019-07-20 22:55:17 +0200

    api error function builduo

--- Platforms/Web/Processing/Api/errors.py
+++ Platforms/Web/Processing/Api/errors.py
@@ -6,6 +6,9 @@ import json
 from aiohttp.web import Response, Request
 
 async def apiUnknown(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs: None
+	"""
 	cls.Web.BASE.Logger.debug(f"(API) 404: {WebRequest.path}", require="api:404")
 	return cls.response(
 		text=json.dumps( dict(error="unknown_api", status=404) ),
@@ -14,14 +17,28 @@ async def apiUnknown(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Respon
 	)
 
 async def apiNothing(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+	"""
+	default_msg:str = "Trying to find out the PhaazeAPI?. Try looking at https://phaaze.net/wiki/api"
+	msg:str = kwargs.get("msg", default_msg)
+
 	cls.Web.BASE.Logger.debug(f"(API) 400: {WebRequest.path}", require="api:400")
 	return cls.response(
-		text=json.dumps( dict(error="no_path", status=400, msg="Trying to find out the PhaazeAPI?. Try looking at phaaze.net/wiki/api") ),
+		text=json.dumps( dict(error="no_path", status=400, msg=msg) ),
 		content_type="application/json",
 		status=400
 	)
 
-async def apiNotAllowed(cls:"WebIndex", WebRequest:Request, msg:str="Not allowed", **kwargs:Any) -> Response:
+async def apiNotAllowed(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+	"""
+	default_msg:str = "Not allowed"
+	msg:str = kwargs.get("msg", default_msg)
+
 	cls.Web.BASE.Logger.debug(f"(API) 403: {WebRequest.path}", require="api:403")
 	return cls.response(
 		text=json.dumps( dict(error="action_not_allowed", status=403, msg=msg) ),
@@ -29,7 +46,14 @@ async def apiNotAllowed(cls:"WebIndex", WebRequest:Request, msg:str="Not allowed
 		status=403
 	)
 
-async def apiMissingValidMethod(cls:"WebIndex", WebRequest:Request, msg:str="Missing valid method", **kwargs:Any) -> Response:
+async def apiMissingValidMethod(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+	"""
+	default_msg:str = "Missing valid method"
+	msg:str = kwargs.get("msg", default_msg)
+
 	cls.Web.BASE.Logger.debug(f"(API) 400: {WebRequest.path}", require="api:400")
 	return cls.response(
 		text=json.dumps( dict(error="missing_valid_method", status=400, msg=msg) ),
@@ -38,6 +62,10 @@ async def apiMissingValidMethod(cls:"WebIndex", WebRequest:Request, msg:str="Mis
 	)
 
 async def apiMissingAuthorisation(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs: None
+	"""
+
 	cls.Web.BASE.Logger.debug(f"(Web/API) Missing Authorisation", require="api:400")
 	return cls.response(
 		status=401,
@@ -46,15 +74,29 @@ async def apiMissingAuthorisation(cls:"WebIndex", WebRequest:Request, **kwargs:A
 	)
 
 async def userNotFound(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+	"""
+	default_msg:str = "No user could be found, check password or username"
+	msg:str = kwargs.get("msg", default_msg)
+
 	cls.Web.BASE.Logger.debug(f"(Web/API) User not found", require="api:400")
 	return cls.response(
-		text=json.dumps( dict(error="no_user_found", status=404, msg="no user could be found, check password or username") ),
+		text=json.dumps( dict(error="no_user_found", status=404, msg=msg) ),
 		content_type="application/json",
 		status=404
 	)
 
-async def missingData(cls:"WebIndex", WebRequest:Request, msg:str="missing required data", **kwargs:Any) -> Response:
-	cls.Web.BASE.Logger.debug(f"(Web/API) User not found", require="api:400")
+async def missingData(cls:"WebIndex", WebRequest:Request, **kwargs:Any) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+	"""
+	default_msg:str = "Missing required data"
+	msg:str = kwargs.get("msg", default_msg)
+
+	cls.Web.BASE.Logger.debug(f"(Web/API) Missing Data for api request", require="api:400")
 	return cls.response(
 		text=json.dumps( dict(error="missing_data", status=404, msg=msg) ),
 		content_type="application/json",
