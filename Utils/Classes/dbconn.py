90eff2660c6c2d51c3e996599734631983812d6e
the-cj <christoperjaekel@googlemail.com>
2019-07-25 19:15:33 +0200

    simplisity changes to db

--- Utils/Classes/dbconn.py
+++ Utils/Classes/dbconn.py
@@ -11,29 +11,42 @@ class DBConn(object):
 		self.passwd:str = passwd
 		self.database:str = database
 
-	def query(self, sql:str, values:tuple or list = None, commit:bool = False) -> list:
+	def query(self, sql:str, values:tuple or list = None) -> list:
 		"""
 			Querys a SQL command.
-			If its a writing action, commit MUST be true.
-			Else no action is taken.
-			Returns a list of dict
+
+			Each query is in thery a transaction,
+			use self.getConnection()
+			to get a own connection object.
+
+			Then use connection.cursor to make all requests,
+			if finished and everything worked without error,
+			use connection.commit() to actully save all changes
 		"""
-		DBConn:MySQLConnection = connect(
+		DBConn:MySQLConnection = self.getConnection()
+
+		Cursor:MySQLCursorDict = DBConn.cursor(dictionary=True)
+		Cursor.execute(sql, values)
+
+		if Cursor._have_unread_result(): res:list = Cursor.fetchall()
+		else: res:list = []
+
+		DBConn.commit()
+		DBConn.close()
+
+		return res
+
+	def getConnection(self) -> MySQLConnection:
+		"""
+			Returns a connection for the db,
+			that can be used for transactions
+			all changes can be undone by connection.rollback()
+
+			finish a connection with commit() and close()
+		"""
+		return connect(
 			host = self.host,
 			user = self.user,
 			passwd = self.passwd,
 			database = self.database
 		)
-
-		Cursor:MySQLCursorDict = DBConn.cursor(dictionary=True)
-
-		Cursor.execute(sql, values)
-
-		if commit:
-			DBConn.commit()
-
-		res:list = Cursor.fetchall()
-
-		DBConn.close()
-
-		return res
