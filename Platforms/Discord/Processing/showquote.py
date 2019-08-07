168896ad4d6ccb6ab2c8e0ddb263e7be0b69c11e
the-cj <christoperjaekel@googlemail.com>
2019-08-07 16:56:43 +0200

    prework for quote select and preline swap

---
+++ Platforms/Discord/Processing/showquote.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Discord.main_discord import PhaazebotDiscord
+
+from Utils.Classes.discordcommand import DiscordCommand
+from Utils.Classes.discordcommandcontext import DiscordCommandContext
+from Platforms.Discord.utils import getDiscordServerCommands
+
+async def showQuote(cls:"PhaazebotDiscord", Command:DiscordCommand, CommandContext:DiscordCommandContext) -> dict:
+
+	command_link:str = f"https://phaaze.net/discord/commands/{Command.server_id}"
+	all_commands:list = await getDiscordServerCommands(cls, Command.server_id)
+
+	finished_str:str = ":link: All commands on this server in one place\n"\
+	f"{command_link}\n"\
+	f"There are {str(len(all_commands))} Command(s) on this server\n"
+
+	finished_str += "\n".join( [f"`{C.trigger.replace('`', '')}`" for C in all_commands[:MAX_SHOW_COMMANDS]] )
+
+	if len(all_commands) > 20:
+		finished_str += "\n and some more"
+
+	return {"content": finished_str}
--- Platforms/Discord/commandindex.py
+++ Platforms/Discord/commandindex.py
@@ -3,20 +3,31 @@ from Utils.Classes.storeclasses import GlobalStorage
 
 from .Processing.textonly import textOnly
 from .Processing.listcommands import listCommands
+from .Processing.showquote import showQuote
 
 command_register:list = [
 	dict(
 		name = "Text dummy",
 		function = textOnly,
-		description = "A simple text dummy that returns a predefined text",
-		details = "This command requires a content, this content supports placeholder variables",
+		description = "A simple text dummy that returns a predefined text, it requires a content,\n"\
+			"this content supports placeholder variables",
+		details = "This command does not take any arguments",
 		need_content = True
 	),
 	dict(
 		name = "List commands",
 		function = listCommands,
-		description = "List all commands a server has, also provides a link to the public command website",
-		details = "It only shows the first 20 commands, after that it only says there are more. Else it would be to spammy",
+		description = "List all commands a server has, also provides a link to the public command website\n"\
+			"It only shows the first 20 commands, after that it only says there are more. Else it would be to spammy",
+		details = "This command does not take any arguments",
+		need_content = False
+	),
+	dict(
+		name = "Show quote",
+		function = showQuote,
+		description = "If not defined otherwise, it returns a random quote from this server",
+		details = "This function takes one 1 optional argument.\n"\
+			"(1) ID of the quote you want.",
 		need_content = False
 	),
 ]
