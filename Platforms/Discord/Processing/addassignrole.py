5a129e181a47101787ac49ec7ba3656ac20c414a
the-cj <christoperjaekel@googlemail.com>
2019-08-28 20:54:16 +0200

    security checks to add assign role

--- Platforms/Discord/Processing/addassignrole.py
+++ Platforms/Discord/Processing/addassignrole.py
@@ -9,8 +9,22 @@ from Platforms.Discord.utils import getDiscordRoleFromString
 
 async def addAssignRole(cls:"PhaazebotDiscord", Command:DiscordCommand, CommandContext:DiscordCommandContext) -> dict:
 
+	Perm:discord.Permissions = CommandContext.Message.channel.permissions_for(CommandContext.Message.guild.me)
+	if not Perm.manage_roles:
+		return {"content": ":no_entry_sign: Phaaze don't has a role with the `Manage Roles` Permission."}
+
 	trigger:str = CommandContext.part(1)
 	query_str:str = " ".join( CommandContext.parts[2:] )
+
+	res:list = cls.BASE.PhaazeDB.query("""
+		SELECT COUNT(*) AS c
+		FROM discord_giverole
+		WHERE discord_giverole.guild_id = %s""",
+		(Command.server_id,)
+	)
+
+	if res[0]["c"] >= cls.BASE.Limit.DISCORD_ADDROLE_AMOUNT:
+		return {"content": ":no_entry_sign: This server hit the assign role limit, please remove some first."}
 
 	if not trigger or not query_str:
 		return {"content": ":warning: You need to define a role-trigger and a role."}
@@ -19,6 +33,9 @@ async def addAssignRole(cls:"PhaazebotDiscord", Command:DiscordCommand, CommandC
 
 	if not AssignRole:
 		return {"content": f":warning: Could not find any role matching: `{query_str}`"}
+
+	if CommandContext.Message.guild.me.top_role < AssignRole:
+		return {"content": f":no_entry_sign: The Role `{AssignRole.name}` is to high. Phaaze highest role has to be higher in hierarchy then `{AssignRole.name}`"}
 
 	cls.BASE.PhaazeDB.query("""
 		INSERT INTO discord_giverole
