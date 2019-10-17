92f9fffc9f1302f03dc54c6f308509f3854f11ca
the-cj <christoperjaekel@googlemail.com>
2019-10-17 22:19:51 +0200

    reworked discord api command edit

--- Platforms/Web/Processing/Api/Discord/Commands/edit.py
+++ Platforms/Web/Processing/Api/Discord/Commands/edit.py
@@ -21,18 +21,9 @@ async def apiDiscordCommandsEdit(cls:"WebIndex", WebRequest:Request) -> Response
 	Data:WebRequestContent = WebRequestContent(WebRequest)
 	await Data.load()
 
-	# get stuff
+	# get required stuff
 	guild_id:str = Data.getStr("guild_id", "", must_be_digit=True)
-	command_id:str = Data.getStr("command_id", "", must_be_digit=True)
-
-	trigger:str = Data.getStr("trigger", "")
-	complex_:bool = Data.getBool("complex", False)
-	function:str = Data.getStr("function", "")
-	content:str = Data.getStr("content", "")
-	hidden:bool = Data.getBool("hidden", False)
-	cooldown:int = Data.getInt("cooldown", 0, min_x=0)
-	require:int = Data.getInt("require", 0, min_x=0)
-	required_currency:int = Data.getInt("required_currency", 0, min_x=0)
+	command_id:str = Data.getStr("command_id", "0", must_be_digit=True)
 
 	# guild id check
 	if not guild_id:
@@ -42,38 +33,83 @@ async def apiDiscordCommandsEdit(cls:"WebIndex", WebRequest:Request) -> Response
 	if not command_id:
 		return await missingData(cls, WebRequest, msg="missing or invalid 'command_id'")
 
-	# only take the first argument of trigger, since everything else can't be typed in a channel
-	trigger = trigger.split(" ")[0]
-	if not trigger:
-		return await missingData(cls, WebRequest, msg="missing 'trigger'")
+	# check if command exists before edit
+	commands:list = await getDiscordServerCommands(cls.Web.BASE.Discord, guild_id, command_id=command_id)
+	if not commands:
+		return await apiDiscordCommandNotExists(cls, WebRequest, command_id=command_id)
 
-	#cooldown
-	if not (cls.Web.BASE.Limit.DISCORD_COMMANDS_COOLDOWN_MIN <= int(cooldown) <= 600 ):
-		return await apiWrongData(cls, WebRequest, msg="'cooldown' is wrong")
+	CurrentEditCommand:DiscordCommand = commands[0]
 
-	#currency
-	if not required_currency >= 0 :
-		return await apiWrongData(cls, WebRequest, msg="'required_currency' is wrong")
+	# after this point we have a existing command
+	# check all possible values if it should be edited
+	update:dict = dict()
 
-	# if not complex
-	# check if the function actully exists
+	# change trigger?
+	trigger:str = Data.getStr("trigger", "")
+	if trigger:
+		# only take the first argument of trigger, since everything else can't be typed in a channel
+		trigger = trigger.split(" ")[0]
+		if not trigger:
+			# should never happen
+			return await missingData(cls, WebRequest, msg="missing 'trigger'")
+
+		# check if there is another command with the trigger that wants to be set, that is NOT the one currently editing
+		check_double_trigger:list = await getDiscordServerCommands(cls.Web.BASE.Discord, guild_id, trigger=trigger)
+		if check_double_trigger:
+			CommandToCheck:DiscordCommand = check_double_trigger[0]
+			if str(CommandToCheck.command_id) != str(CurrentEditCommand.command_id):
+				return await apiDiscordCommandExists(cls, WebRequest, command=trigger)
+
+		update["trigger"] = trigger
+
+	# change cooldown
+	cooldown:int = Data.getInt("cooldown", None, min_x=0)
+	if cooldown != None:
+		# wants a invalid cooldown
+		if not (cls.Web.BASE.Limit.DISCORD_COMMANDS_COOLDOWN_MIN <= cooldown <= cls.Web.BASE.Limit.DISCORD_COMMANDS_COOLDOWN_MAX ):
+			return await apiWrongData(cls, WebRequest, msg="'cooldown' is wrong")
+
+		update["cooldown"] = cooldown
+
+	# change currency
+	required_currency:int = Data.getInt("required_currency", None, min_x=0)
+	if required_currency != None:
+		update["required_currency"] = required_currency
+
+	# change require
+	require:int = Data.getInt("require", None, min_x=0)
+	if require != None :
+		update["require"] = require
+
+	# change content
+	content:str = Data.getStr("content", None)
+	if content != None:
+		update["content"] = content
+
+	# change hidden
+	hidden:bool = Data.getBool("hidden", None)
+	if hidden != None:
+		update["hidden"] = hidden
+
+	# change function
+	complex_:bool = Data.getBool("complex", False)
+	function:str = Data.getStr("function", "")
 	if not complex_:
+		# it its not complex, we need a function
 		if not function:
 			return await missingData(cls, WebRequest, msg="missing 'function'")
 		if not function in [cmd["function"].__name__ for cmd in command_register]:
 			return await apiWrongData(cls, WebRequest, msg=f"'{function}' is not a valid value for field 'function'")
 
-	# check if command exists before edit
-	commands:list = await getDiscordServerCommands(cls.Web.BASE.Discord, guild_id, command_id=command_id)
-	if not commands:
-		return await apiDiscordCommandNotExists(cls, WebRequest, command=trigger)
+		update["complex"] = complex_
+		update["function"] = function
 
-	# check if there is another command with the trigger that wants to be set, that is NOT the one currently editing
-	check_double_trigger:list = await getDiscordServerCommands(cls.Web.BASE.Discord, guild_id, trigger=trigger)
-	if check_double_trigger:
-		CommandToCheck:DiscordCommand = check_double_trigger[0]
-		if str(CommandToCheck.command_id) != str(command_id):
-			return await apiDiscordCommandExists(cls, WebRequest, command=trigger)
+	else:
+		# it is complex, we need a content
+		if not content:
+			return await apiWrongData(cls, WebRequest, msg=f"'complex' is true, but missing 'content'")
+
+	# all checks done, authorise the user
 
 	# get/check discord
 	PhaazeDiscord:"PhaazebotDiscord" = cls.Web.BASE.Discord
@@ -95,16 +131,8 @@ async def apiDiscordCommandsEdit(cls:"WebIndex", WebRequest:Request) -> Response
 	if not (CheckMember.guild_permissions.administrator or CheckMember.guild_permissions.manage_guild):
 		return await apiDiscordMissingPermission(cls, WebRequest, guild_id=guild_id, user_id=DiscordUser.user_id)
 
-	update:dict = dict(
-		trigger = trigger,
-		complex = complex_,
-		function = function,
-		require = require,
-		required_currency = required_currency,
-		hidden = hidden,
-		content = content,
-		cooldown = cooldown
-	)
+	if not update:
+		return await apiWrongData(cls, WebRequest, msg=f"no changes requested")
 
 	cls.Web.BASE.PhaazeDB.updateQuery(
 		table = "discord_command",
@@ -113,7 +141,7 @@ async def apiDiscordCommandsEdit(cls:"WebIndex", WebRequest:Request) -> Response
 		where_values = (guild_id, command_id)
 	)
 
-	cls.Web.BASE.Logger.debug(f"(API/Discord) Edited command: S:{guild_id} T:{trigger}", require="discord:commands")
+	cls.Web.BASE.Logger.debug(f"(API/Discord) Edited command: S:{guild_id} T:{trigger} U:{str(update)}", require="discord:commands")
 
 	return cls.response(
 		text=json.dumps( dict(msg="new command successfull edited", command=trigger, status=200) ),
