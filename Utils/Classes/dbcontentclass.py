9e996bd1c1524a285a0aa8a383133f5a40098636
the-cj <christoperjaekel@googlemail.com>
2019-11-07 19:17:20 +0100

    parser for string lists

--- Utils/Classes/dbcontentclass.py
+++ Utils/Classes/dbcontentclass.py
@@ -7,7 +7,12 @@ class DBContentClass(object):
 		to create usable classes
 	"""
 
-	def fromJsonField(self, data:str or bytes or None):
-		if type(data) == Undefined: return data
-		if not data: return None
+	def fromJsonField(self, data:str or bytes or None) -> list:
+		if type(data) == Undefined: return list()
+		if not data: return list()
 		return json.loads(data)
+
+	def fromStringList(self, data:str or bytes or None, seperator:str=",") -> list:
+		if type(data) == Undefined: return list()
+		if not data: return list()
+		return data.split(seperator)
