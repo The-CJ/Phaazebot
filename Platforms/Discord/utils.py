d69b189d0e6d2fa57a6ab92d74a24d392d53be74
the-cj <christoperjaekel@googlemail.com>
2019-11-10 01:07:33 +0100

    new tables and gather options

--- Platforms/Discord/utils.py
+++ Platforms/Discord/utils.py
@@ -41,7 +41,11 @@ async def getDiscordSeverSettings(cls:"PhaazebotDiscord", origin:discord.Message
 			(SELECT GROUP_CONCAT(`discord_disabled_normalchannel`.`channel_id` SEPARATOR ',') FROM `discord_disabled_normalchannel` WHERE `discord_disabled_normalchannel`.`guild_id` = `discord_setting`.`guild_id`)
 				AS `disabled_normalchannels`,
 			(SELECT GROUP_CONCAT(`discord_disabled_regularchannel`.`channel_id` SEPARATOR ',') FROM `discord_disabled_regularchannel` WHERE `discord_disabled_regularchannel`.`guild_id` = `discord_setting`.`guild_id`)
-				AS `disabled_regularchannels`
+				AS `disabled_regularchannels`,
+			(SELECT GROUP_CONCAT(`discord_enabled_gamechannel`.`channel_id` SEPARATOR ',') FROM `discord_enabled_gamechannel` WHERE `discord_enabled_gamechannel`.`guild_id` = `discord_setting`.`guild_id`)
+				AS `enabled_gamechannels`,
+			(SELECT GROUP_CONCAT(`discord_enabled_nsfwchannel`.`channel_id` SEPARATOR ',') FROM `discord_enabled_nsfwchannel` WHERE `discord_enabled_nsfwchannel`.`guild_id` = `discord_setting`.`guild_id`)
+				AS `enabled_nswfchannels`
 
 		FROM `discord_setting`
 		WHERE `discord_setting`.`guild_id` = %s
