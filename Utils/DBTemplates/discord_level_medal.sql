71c650472b651f797e2b04d662c64577eb9fa636
the-cj <christoperjaekel@googlemail.com>
2019-11-14 20:16:13 +0100

    changes for discord medals

--- Utils/DBTemplates/discord_level_medal.sql
+++ Utils/DBTemplates/discord_level_medal.sql
@@ -1,5 +1,5 @@
 /*
-DESCRIBE `discord_level`;
+DESCRIBE `discord_level_medal`;
 
 +-----------+--------------+------+-----+---------+----------------+
 | Field     | Type         | Null | Key | Default | Extra          |
@@ -13,15 +13,13 @@ DESCRIBE `discord_level`;
 +-----------+--------------+------+-----+---------+----------------+
 */
 
--- SHOW CREATE TABLE `discord_level`;
+-- SHOW CREATE TABLE `discord_level_medal`;
 
-CREATE TABLE `discord_level` (
+CREATE TABLE `discord_level_medal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `guild_id` varchar(128) NOT NULL,
   `member_id` varchar(128) NOT NULL,
-  `edited` tinyint(1) DEFAULT 0,
-  `exp` int(8) DEFAULT 0,
-  `on_server` tinyint(1) DEFAULT 1,
+  `name` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `guild_id` (`guild_id`,`member_id`)
+  UNIQUE KEY `guild_id` (`guild_id`,`member_id`, `name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
