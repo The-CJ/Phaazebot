e5fb71b0a2de1d475ea9bd598a3bc7df3b57aa2e
the-cj <christoperjaekel@googlemail.com>
2019-11-21 00:43:01 +0100

    new error class for un found quotes

--- Platforms/Web/Processing/Api/Discord/errors.py
+++ Platforms/Web/Processing/Api/Discord/errors.py
@@ -133,7 +133,7 @@ async def apiDiscordChannelNotFound(cls:"WebIndex", WebRequest:Request, **kwargs
 		status=404
 	)
 
-# non exists
+# not/already exists
 async def apiDiscordCommandExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
 	"""
 		Takes from kwargs:
@@ -172,6 +172,29 @@ async def apiDiscordCommandNotExists(cls:"WebIndex", WebRequest:Request, **kwarg
 	command:str = kwargs.get("command", "")
 	if command:
 		res["command"] = command
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Command not found: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
+
+async def apiDiscordQuotesNotExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+			quote_id:str
+	"""
+	res:dict = dict(status=400)
+
+	default_msg:str = "No quote has been found"
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	quote_id:str = kwargs.get("quote_id", "")
+	if quote_id:
+		res["quote_id"] = quote_id
 
 	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Command not found: {WebRequest.path}", require="api:400")
 	return cls.response(
