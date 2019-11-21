503de62c72fffa0e1a53f877ca9b3ca39e4aa71c
The_CJ <cj@tbits.net>
2019-11-21 10:47:27 +0100

    rewrite for my code rules

--- Utils/logger.py
+++ Utils/logger.py
@@ -8,8 +8,8 @@ except ImportError:	pass
 
 class PhaazeLoggerFormatter(logging.Formatter):
 	""" custom logging colors """
-	default_color = "\033[00m"
-	colors = dict(
+	default_color:str = "\033[00m"
+	colors:dict = dict(
 		DEBUG = "\033[90m",
 		INFO = "\033[36m",
 		WARNING = "\033[93m",
@@ -17,12 +17,11 @@ class PhaazeLoggerFormatter(logging.Formatter):
 		CRITICAL = "\033[31m",
 	)
 
-	def formatMessage(self, record) -> None:
-		lvl = record.levelname
-		s = self.colors.get(lvl, self.default_color)
-		e = self.default_color
-		record.levelname = f"{s}{lvl}{e}"
-		return self._style.format(record)
+	def formatMessage(self, Record:logging.LogRecord) -> None:
+		lvl:str = Record.levelname
+		wanted_color:str = self.colors.get(lvl, self.default_color)
+		Record.levelname = f"{wanted_color}{lvl}{self.default_color}"
+		return self._style.format(Record)
 
 class PhaazeLogger(object):
 	""" Logger for project, sends to systemd or console """
@@ -35,14 +34,15 @@ class PhaazeLogger(object):
 		self.logging_type:str = CliArgs.get("logging" , "console")
 
 		if self.logging_type in ["systemd", "cysystemd"] and "cysystemd" in sys.modules:
+			# TODO: rework this
 			JH = JournaldLogHandler()
 			JH.setFormatter(self.Formatter)
 			self.Log.addHandler(JH)
 
 		elif self.logging_type == "console":
-			SH = logging.StreamHandler()
-			SH.setFormatter(self.Formatter)
-			self.Log.addHandler(SH)
+			PhaazeStreamHandler:logging.StreamHandler = logging.StreamHandler()
+			PhaazeStreamHandler.setFormatter(self.Formatter)
+			self.Log.addHandler(PhaazeStreamHandler)
 
 		else:
 			pass
@@ -67,7 +67,9 @@ class PhaazeLogger(object):
 			if require.split(":")[0] == ad: show = True; break
 
 		if show:
+			# Caller tracks back the command that called this function,
+			# i only take the line, and the file in which it happen
 			Caller:inspect.Traceback = inspect.getframeinfo(inspect.stack()[1][0])
-
 			location:str = Caller.filename.replace(os.getcwd(), "")
+
 			self.Log.debug(f"{location}:{Caller.lineno} | {msg}")
