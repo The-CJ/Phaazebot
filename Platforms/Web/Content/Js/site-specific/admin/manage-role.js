0a5f30426ec101f98546edf398dffebe3faa98df
The_CJ <cj@tbits.net>
2019-11-21 11:23:07 +0100

    prework frontend ask

--- Platforms/Web/Content/Js/site-specific/admin/manage-role.js
+++ Platforms/Web/Content/Js/site-specific/admin/manage-role.js
@@ -1,3 +1,17 @@
 $("document").ready(function () {
-
+  getRoles();
 })
+
+function getRoles() {
+  $.get("/api/admin/roles/get")
+  .done(function (data) {
+
+    console.log(data);
+
+  })
+  .fail(function (data) {
+    let msg = data.responseJSON ? data.responseJSON.error : "unknown";
+    Display.showMessage( {content:msg, color:Display.color_critical} );
+    console.log(data);
+  })
+}
