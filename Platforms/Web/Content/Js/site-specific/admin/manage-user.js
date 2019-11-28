6343d0a1483dc304739e2af3c92679c003b12d8e
The_CJ <cj@tbits.net>
2019-11-28 11:16:00 +0100

    more prework and testing in frontend

--- Platforms/Web/Content/Js/site-specific/admin/manage-user.js
+++ Platforms/Web/Content/Js/site-specific/admin/manage-user.js
@@ -1,3 +1,26 @@
 $("document").ready(function () {
-
+  getUser();
 });
+
+function getUser() {
+  $.get("/api/admin/users/get")
+  .done(function (data) {
+
+    var UserList = $("#user_list").html("");
+
+    for (var user of data.result) {
+      console.log(user);
+      // var Template = $("[phantom] .role").clone();
+
+      // Template.attr("role-id", role.id);
+      // Template.find(".name").text(role.name);
+      // Template.find("[can_be_removed]").attr("can_be_removed", role.can_be_removed ? "true" : "false");
+
+      // RoleList.append(Template);
+    }
+
+  })
+  .fail(function (data) {
+    generalAPIErrorHandler( {data:data, msg:"can't load user"} );
+  })
+}
