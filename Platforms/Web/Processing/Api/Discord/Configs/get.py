aa8e61e792e946af90d4fd72d50ba5a2ad7b5f5f
the-cj <christoperjaekel@googlemail.com>
2019-11-30 22:31:22 +0100

    Discord Settings toJSON

--- Platforms/Web/Processing/Api/Discord/Configs/get.py
+++ Platforms/Web/Processing/Api/Discord/Configs/get.py
@@ -49,38 +49,8 @@ async def apiDiscordConfigsGet(cls:"WebIndex", WebRequest:Request) -> Response:
 	if not Configs:
 		return await apiDiscordGuildUnknown(cls, WebRequest, msg="Could not find configs for this guild")
 
-	conf:dict = dict(
-		autorole = None,
-		blacklist_ban_links = Configs.blacklist_ban_links,
-		blacklist_whitelistroles = Configs.blacklist_whitelistroles,
-		blacklist_whitelistlinks = Configs.blacklist_whitelistlinks,
-		blacklist_blacklistwords = Configs.blacklist_blacklistwords,
-		blacklist_punishment = Configs.blacklist_punishment,
-		currency_name = Configs.currency_name,
-		currency_name_multi = Configs.currency_name_multi,
-		disabled_levelchannels = Configs.disabled_levelchannels,
-		disabled_quotechannels = Configs.disabled_quotechannels,
-		disabled_normalchannels = Configs.disabled_normalchannels,
-		disabled_regularchannels = Configs.disabled_regularchannels,
-		enabled_gamechannels = Configs.enabled_gamechannels,
-		enabled_nsfwchannels = Configs.enabled_nsfwchannels,
-		level_announce_chan = Configs.level_announce_chan,
-		level_custom_msg = Configs.level_custom_msg,
-		leave_msg = Configs.leave_msg,
-		leave_chan = Configs.leave_chan,
-		owner_disable_level = Configs.owner_disable_level,
-		owner_disable_normal = Configs.owner_disable_normal,
-		owner_disable_regular = Configs.owner_disable_regular,
-		owner_disable_mod = Configs.owner_disable_mod,
-		track_channel = Configs.track_channel,
-		track_options = Configs.track_options,
-		welcome_chan = Configs.welcome_chan,
-		welcome_msg = Configs.welcome_msg,
-		welcome_msg_priv = Configs.welcome_msg_priv,
-	)
-
 	return cls.response(
-		text=json.dumps( dict(result=conf, status=200) ),
+		text=json.dumps( dict(result=Configs.toJSON(), status=200) ),
 		content_type="application/json",
 		status=200
 	)
--- Utils/Classes/discordserversettings.py
+++ Utils/Classes/discordserversettings.py
@@ -48,3 +48,38 @@ class DiscordServerSettings(DBContentClass):
 			return f"<{self.__class__.__name__} guild_id='{self.server_id}'>"
 		else:
 			return f"<{self.__class__.__name__} Empty configs>"
+
+	def toJSON(self) -> dict:
+		""" Returns a json save dict representation of all values for API, storage, etc... """
+
+		j:dict = dict()
+
+		j["autorole_id"] = self.autorole_id
+		j["blacklist_ban_links"] = self.blacklist_ban_links
+		j["blacklist_whitelistroles"] = self.blacklist_whitelistroles
+		j["blacklist_whitelistlinks"] = self.blacklist_whitelistlinks
+		j["blacklist_blacklistwords"] = self.blacklist_blacklistwords
+		j["blacklist_punishment"] = self.blacklist_punishment
+		j["currency_name"] = self.currency_name
+		j["currency_name_multi"] = self.currency_name_multi
+		j["disabled_levelchannels"] = self.disabled_levelchannels
+		j["disabled_quotechannels"] = self.disabled_quotechannels
+		j["disabled_normalchannels"] = self.disabled_normalchannels
+		j["disabled_regularchannels"] = self.disabled_regularchannels
+		j["enabled_gamechannels"] = self.enabled_gamechannels
+		j["enabled_nsfwchannels"] = self.enabled_nsfwchannels
+		j["level_announce_chan"] = self.level_announce_chan
+		j["level_custom_msg"] = self.level_custom_msg
+		j["leave_msg"] = self.leave_msg
+		j["leave_chan"] = self.leave_chan
+		j["owner_disable_level"] = self.owner_disable_level
+		j["owner_disable_normal"] = self.owner_disable_normal
+		j["owner_disable_regular"] = self.owner_disable_regular
+		j["owner_disable_mod"] = self.owner_disable_mod
+		j["track_channel"] = self.track_channel
+		j["track_options"] = self.track_options
+		j["welcome_chan"] = self.welcome_chan
+		j["welcome_msg"] = self.welcome_msg
+		j["welcome_msg_priv"] = self.welcome_msg_priv
+
+		return j
