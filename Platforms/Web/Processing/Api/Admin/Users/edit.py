da89524b56259888cf2ade4f77aff0abe7109967
the-cj <christoperjaekel@googlemail.com>
2019-12-01 04:41:16 +0100

    backend role add/remove

--- Platforms/Web/Processing/Api/Admin/Users/edit.py
+++ Platforms/Web/Processing/Api/Admin/Users/edit.py
@@ -5,6 +5,7 @@ if TYPE_CHECKING:
 import json
 from aiohttp.web import Response, Request
 from Utils.Classes.webrequestcontent import WebRequestContent
+from Utils.Classes.webrole import WebRole
 from Platforms.Web.Processing.Api.errors import missingData, apiWrongData
 from Utils.Classes.undefined import UNDEFINED
 from Utils.dbutils import validateDBInput
@@ -21,7 +22,9 @@ async def apiAdminUsersEdit(cls:"WebIndex", WebRequest:Request) -> Response:
 		return await missingData(cls, WebRequest, msg="missing or invalid 'user_id'")
 
 	# single actions
-	# TODO: s
+	action:str = Data.getStr("userrole_action", UNDEFINED)
+	if action:
+		return await singleActionUserRole(cls, WebRequest, action, Data)
 
 	changes:dict = dict()
 	db_changes:dict = dict()
@@ -48,18 +51,86 @@ async def apiAdminUsersEdit(cls:"WebIndex", WebRequest:Request) -> Response:
 		return await missingData(cls, WebRequest, msg="No changes, please add at least one")
 
 	cls.Web.BASE.Logger.debug(f"(API) Config User U:{user_id} {str(db_changes)}", require="api:user")
-	hits:int = cls.Web.BASE.PhaazeDB.updateQuery(
+	cls.Web.BASE.PhaazeDB.updateQuery(
 		table = "user",
 		content = db_changes,
 		where = "user.id = %s",
 		where_values = (user_id,)
 	)
 
-	if not hits:
-		return await apiWrongData(cls, WebRequest, msg="no users updated, 'user_id' not found")
-
 	return cls.response(
 		text=json.dumps( dict(msg="user successfull updated", changes=changes, status=200) ),
 		content_type="application/json",
 		status=200
 	)
+
+async def singleActionUserRole(cls:"WebIndex", WebRequest:Request, action:str, Data:WebRequestContent) -> Response:
+	"""
+		Default url: /api/admin/users/edit?userrole_action=something
+	"""
+	user_id:str = Data.getStr("user_id", "")
+	action = action.lower()
+	userrole_role:str = Data.getStr("userrole_role", "").strip(" ").strip("\n")
+
+	if not user_id:
+		# should never happen
+		return await missingData(cls, WebRequest, msg="missing field 'user_id'")
+
+	if not userrole_role:
+		return await missingData(cls, WebRequest, msg="missing or invalid field 'userrole_role'")
+
+	# if it's not a number, try to get id from name, else... just select it again... REEEE
+	res:list = cls.Web.BASE.PhaazeDB.selectQuery("""
+		SELECT *
+		FROM `role`
+		WHERE LOWER(`role`.`name`) = LOWER(%s)
+			OR `role`.`id` = %s""",
+		(userrole_role, userrole_role)
+	)
+
+	if not res:
+		return await apiWrongData(cls, WebRequest, msg=f"role '{userrole_role}' could not be resolved as a role")
+
+	# here we hope there is only one result, in theory there can be more, BUT since this is a admin endpoint,
+	# i wont will do much error handling
+	Role:WebRole = WebRole(res.pop(0))
+
+	if action == "add":
+		try:
+			cls.Web.BASE.PhaazeDB.insertQuery(
+				table = "user_has_role",
+				content = dict(
+					user_id = user_id,
+					role_id = Role.role_id
+				)
+			)
+			return cls.response(
+				text=json.dumps( dict(msg="user roles successfull updated", add=Role.name, status=200) ),
+				content_type="application/json",
+				status=200
+			)
+		except:
+			return await apiWrongData(cls, WebRequest, msg=f"user already has role: '{Role.name}'")
+
+	elif action == "remove":
+		try:
+			hits:int = cls.Web.BASE.PhaazeDB.deleteQuery("""
+				DELETE FROM `user_has_role`
+				WHERE `role_id` = %s
+					AND `user_id` = %s""",
+				(Role.role_id, user_id)
+			)
+
+			if not hits:
+				raise RuntimeError()
+
+			return cls.response(
+				text=json.dumps( dict(msg="user roles successfull updated", rem=Role.name, status=200) ),
+				content_type="application/json",
+				status=200
+			)
+		except:
+			return await apiWrongData(cls, WebRequest, msg=f"user don't has role: '{Role.name}'")
+
+	else:
+		return await apiWrongData(cls, WebRequest)
