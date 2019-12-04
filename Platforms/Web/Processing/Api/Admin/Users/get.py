e80106d812196abb76e3a1da9912713afe6849b1
The_CJ <cj@tbits.net>
2019-12-04 14:16:15 +0100

    get now with search support

--- Platforms/Web/Processing/Api/Admin/Users/get.py
+++ Platforms/Web/Processing/Api/Admin/Users/get.py
@@ -6,7 +6,7 @@ import json
 from aiohttp.web import Response, Request
 from Platforms.Web.Processing.Api.errors import userNotFound
 from Utils.Classes.webrequestcontent import WebRequestContent
-from Platforms.Web.utils import searchUser
+from Platforms.Web.utils import searchUser, getWebUserAmount
 
 async def apiAdminUsersGet(cls:"WebIndex", WebRequest:Request) -> Response:
 	"""
@@ -16,19 +16,36 @@ async def apiAdminUsersGet(cls:"WebIndex", WebRequest:Request) -> Response:
 	await Data.load()
 
 	user_id:str = Data.getStr("user_id", "", must_be_digit=True)
-	where:str = f"`user`.`id` = {user_id}" if user_id else "1=1"
+	username:str = Data.getStr("username", "")
+	email:str = Data.getStr("email", "")
 
-	users:list = await searchUser(cls, where=where)
+	offset:str = Data.getStr("offset", "", must_be_digit=True)
+	limit:str = Data.getStr("limit", "", must_be_digit=True)
+
+	where:str = "1=1"
+	values:tuple = ()
+
+	if user_id:
+		where = "`user`.`id` = %s"
+		values = (user_id,)
+
+	elif email or username:
+		where = "`user`.`username` LIKE %s OR `user`.`email` LIKE %s"
+		values = (username, email)
+
+	users:list = await searchUser(cls, where=where, values=values, limit=limit, offset=offset)
 
 	if not users:
-		return await userNotFound(cls, WebRequest, msg=f"no user found with id: '{user_id}'")
+		return await userNotFound(cls, WebRequest, msg=f"no user found")
 
-	return_list:list = list()
-	for WebUser in users:
-		return_list.append(WebUser.toJSON())
+	result:dict = dict(
+		result=[ WebUser.toJSON() for WebUser in users ],
+		total = await getWebUserAmount(cls),
+		status=200
+	)
 
 	return cls.response(
-		text=json.dumps( dict(result=return_list, status=200) ),
+		text=json.dumps( result ),
 		content_type="application/json",
 		status=200
 	)
