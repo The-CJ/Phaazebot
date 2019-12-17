5236e24a0e0d06eb15f61d8fbe17ffbcc7a1124d
the-cj <christoperjaekel@googlemail.com>
2019-12-17 18:00:07 +0100

    new error

--- Platforms/Web/Processing/Api/Discord/Assignroles/errors.py
+++ Platforms/Web/Processing/Api/Discord/Assignroles/errors.py
@@ -55,3 +55,26 @@ async def apiDiscordAssignRoleExists(cls:"WebIndex", WebRequest:Request, **kwarg
 		content_type="application/json",
 		status=400
 	)
+
+async def apiDiscordAssignRoleNotExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+			assignrole_id:str
+	"""
+	res:dict = dict(status=400, error="discord_assignrole_not_exists")
+
+	default_msg:str = "Assignrole does not exists"
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	assignrole_id:str = kwargs.get("assignrole_id", "")
+	if assignrole_id:
+		res["assignrole_id"] = assignrole_id
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Assignrole not exists: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
