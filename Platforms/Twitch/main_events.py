6bb69374dc0d322ed70cbf932572e213ba6b956f
the-cj <christoperjaekel@googlemail.com>
2019-12-28 22:02:34 +0100

    base sql getters

--- Platforms/Twitch/main_events.py
+++ Platforms/Twitch/main_events.py
@@ -3,6 +3,7 @@ if TYPE_CHECKING:
 	from main import Phaazebot
 
 import asyncio
+from Platforms.Twitch.api import getTwitchStreams
 
 class PhaazebotTwitchEvents(object):
 	"""
@@ -17,8 +18,8 @@ class PhaazebotTwitchEvents(object):
 	def __init__(self, BASE):
 		self.BASE:"Phaazebot" = BASE
 		self.running:bool = False
-		self.refresh_time:int = 50
-		self.refresh_time_multi:float = 1.0
+		self._refresh_time:int = 50
+		self._refresh_time_multi:float = 1.0
 
 	def stop(self) -> None:
 		if not self.running: raise Exception("not running")
@@ -32,10 +33,39 @@ class PhaazebotTwitchEvents(object):
 		while self.running and self.BASE.Active.twitch_events:
 			self.BASE.Logger.debug("Running new twitch event check...", require="twitch:events")
 
-			self.refresh_time_multi = 1.0 # this can get changed in a check cycle, because of crashes or, null results
+			self._refresh_time_multi = 1.0 # this can get changed in a check cycle, because of crashes or, null results
 			await self.check()
 
-			await asyncio.sleep( self.refresh_time * self.refresh_time_multi )
+			await asyncio.sleep( self.delay )
+
+	@property
+	def delay(self) -> int:
+		return int(self._refresh_time * self._refresh_time_multi)
 
 	async def check(self) -> None:
-		pass
+			# to reduce twitch requests as much as possible, we only ask channels,
+			# that have at least one discord channel alert or have Phaaze in the twitch channel active
+			res:list = self.BASE.PhaazeDB.selectQuery("""
+				SELECT DISTINCT
+				  `twitch_channel`.`channel_id` AS `channel_id`
+				FROM `twitch_channel`
+				WHERE `twitch_channel`.`managed` = 1
+				UNION DISTINCT
+				SELECT DISTINCT
+				  `discord_twitch_alert`.`twitch_channel_id` AS `channel_id`
+				FROM `discord_twitch_alert`"""
+			)
+
+			if not res:
+				# no alerts at all, skip everything and try again much later
+				# this will happen close to never
+				self._refresh_time_multi = 10.0
+				self.BASE.Logger.debug(f"No action, delaying next check ({self.delay}s)", require="twitch:events")
+				return
+
+			try:
+				id_list:list = [ x["channel_id"] for x in res ]
+				current_live_streams = await getTwitchStreams(self.BASE, id_list, limit=1)
+
+			except:
+				pass
