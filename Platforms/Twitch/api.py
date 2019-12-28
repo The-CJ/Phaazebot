163d248394cbe321948d8592a1bacbaf905247a7
the-cj <christoperjaekel@googlemail.com>
2019-12-28 22:52:03 +0100

    forget mass request for now

--- Platforms/Twitch/api.py
+++ Platforms/Twitch/api.py
@@ -3,7 +3,7 @@ if TYPE_CHECKING:
 	from main import Phaazebot
 
 import requests
-import json
+from Utils.Classes.twitchstream import TwitchStream
 
 ROOT_URL = "https://api.twitch.tv/helix/"
 
@@ -15,29 +15,21 @@ async def twitchAPICall(cls:"Phaazebot", url:str) -> requests.Response:
 	header:dict = {"Client-ID": cls.Access.TWITCH_API_TOKEN}
 	return requests.get(url, headers=header)
 
-async def getTwitchStreams(cls:"Phaazebot", item:str or list, item_type:str="user_id", limit:int=100, pagination:str=None) -> list:
+async def getTwitchStreams(cls:"Phaazebot", item:str or list, item_type:str="user_id", limit:int=100) -> list:
 	"""
 		get all currently live streams based on 'stream' and 'search'
 		Returns a list of TwitchStream()
 
 		item [required]
 		item_type : what are the contains of `item` (user_id, user_login, game_id, language)
-		limit : max result number (100 is max)
-		pagination : don't think about this, its for mass requests
+		limit : max result number
 	"""
 
 	if type(item) is not list: item = [item]
 
-	# mass request
-	if len(item) > 100 and (limit > 100 or limit == 0):
-		# TODO: mass requests...
-		pass
-
 	if limit > 100: limit = 100
 
-	query:str = f"?first={limit}&"
-	if pagination:
-		query += f"?after={pagination}"
+	query:str = f"?first={limit}"
 
 	for i in item:
 		query += f"&{item_type}={i}"
