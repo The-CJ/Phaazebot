5c30f0612d5abeb0f3fe90bb251cb4c9c3434f73
the-cj <christoperjaekel@googlemail.com>
2019-12-28 20:30:55 +0100

    some more nots

--- Platforms/Discord/api.py
+++ Platforms/Discord/api.py
@@ -8,6 +8,11 @@ from aiohttp.web import Request
 ROOT_URL = "https://discordapp.com/api/v6/"
 
 async def translateDiscordToken(cls:"Phaazebot", WebRequest:Request) -> dict or None:
+	"""
+		Used to complete a oauth verification via a token the user provies in his GET query
+		(It has to be there)
+		We then get all infos we want/need from discord
+	"""
 	code:str = WebRequest.query.get("code", None)
 	if not code:
 		cls.Logger.debug("translateDiscordToken called without code", require="discord:api")
@@ -26,7 +31,9 @@ async def translateDiscordToken(cls:"Phaazebot", WebRequest:Request) -> dict or 
 	return res.json()
 
 async def getDiscordUser(cls:"Phaazebot", access_token:str) -> dict:
-
+	"""
+		get all infos discord allowes us to see for a user
+	"""
 	headers:dict = {"Authorization": f"Bearer {access_token}"}
 
 	res:requests.Response = requests.get(ROOT_URL+"users/@me", headers=headers)
@@ -34,7 +41,10 @@ async def getDiscordUser(cls:"Phaazebot", access_token:str) -> dict:
 	return res.json()
 
 async def getDiscordUserServers(cls:"Phaazebot", access_token:str) -> list:
-
+	"""
+		get all base infos of guilds/servers a user is on
+		(requires the access_token to have the right scope so we are allowed to see it)
+	"""
 	headers:dict = {"Authorization": f"Bearer {access_token}"}
 
 	res:requests.Response = requests.get(ROOT_URL+"users/@me/guilds", headers=headers)
