e5119e8e0b58eddcf0df01455456f3133107df03
the-cj <christoperjaekel@googlemail.com>
2019-12-28 18:01:01 +0100

    twitch event structure prework

--- Platforms/Twitch/main_events.py
+++ Platforms/Twitch/main_events.py
@@ -2,7 +2,9 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
 	from main import Phaazebot
 
-class PhaazebotTwitchEvents():
+import asyncio
+
+class PhaazebotTwitchEvents(object):
 	"""
 		This Module is to keep track of all events related to twitch
 		and is a info point for other modules so we can reduce twitch api calls as much as possible.
@@ -15,6 +17,8 @@ class PhaazebotTwitchEvents():
 	def __init__(self, BASE):
 		self.BASE:"Phaazebot" = BASE
 		self.running:bool = False
+		self.refresh_time:int = 50
+		self._live_channel_id:list = list()
 
 	def stop(self) -> None:
 		if not self.running: raise Exception("not running")
@@ -23,3 +27,7 @@ class PhaazebotTwitchEvents():
 	async def start(self) -> None:
 		if self.running: raise Exception("already running")
 		self.running = True
+		self.BASE.Logger.info("Started Twitch Events")
+
+		while self.running and self.BASE.Active.twitch_events:
+			await asyncio.sleep(self.refresh_time)
