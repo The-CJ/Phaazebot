5556a310c2cbbfcc117111537f65ef55f05160d8
The_CJ <cj@tbits.net>
2020-01-30 14:51:18 +0100

    ajusted login page/script

--- Platforms/Web/Content/Js/site-specific/account/login.js
+++ Platforms/Web/Content/Js/site-specific/account/login.js
@@ -8,10 +8,10 @@ function loginAccount() {
       window.location = "/account";
     }, 1000);
   }).fail(function (data) {
-    $("#login_space [name=phaaze_username]").addClass("animated shake");
-    $("#login_space [name=phaaze_password]").addClass("animated shake").val("");
+    $("#login_space [name=username]").addClass("animated shake");
+    $("#login_space [name=password]").addClass("animated shake").val("");
     setTimeout(function () {
-      $("#login_space [name=phaaze_username], #login_space [name=phaaze_password]").removeClass("animated shake");
+      $("#login_space [name]").removeClass("animated shake");
     }, 1000);
     generalAPIErrorHandler( {data:data, msg:"Login failed", color:Display.color_warning} );
   })
