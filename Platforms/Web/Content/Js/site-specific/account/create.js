0d38f0ead88a90436cd2be5c253e48d1f2b0adb0
The_CJ <cj@tbits.net>
2020-01-30 15:06:52 +0100

    rewrite scripts and style for create page

--- Platforms/Web/Content/Js/site-specific/account/create.js
+++ Platforms/Web/Content/Js/site-specific/account/create.js
@@ -1,10 +1,12 @@
 function createAccount() {
 
-  let data = extractData('#register_space');
+  let register = extractData('#register_space');
 
-  $.post('/api/account/phaaze/create', data).done(function (data) {
-    window.location = "/account/login?new";
-  }).fail(function (data) {
+  $.post('/api/account/phaaze/create', register)
+  .done(function (data) {
+    window.location = "/account/login";
+  })
+  .fail(function (data) {
     generalAPIErrorHandler( {data:data, msg:"Creation failed", color:Display.color_warning} );
   })
 }
