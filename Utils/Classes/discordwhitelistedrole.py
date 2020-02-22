fbaad0f919d404ac94a9cd2ff1654a265f652942
The_CJ <christoperjaekel@googlemail.com>
2020-02-23 00:45:48 +0100

    new class for exception roles / whitelisted roles

---
+++ Utils/Classes/discordwhitelistedrole.py
@@ -0,0 +1,27 @@
+from Utils.Classes.undefined import UNDEFINED
+from Utils.Classes.dbcontentclass import DBContentClass
+from Utils.Classes.apiclass import APIClass
+
+class DiscordWhitelistedRole(DBContentClass, APIClass):
+	"""
+		Contains and represents a whitelisted role in discord
+		Whitelisted means there are a exception to the blacklist and spam filter
+	"""
+	def __init__(self, data:dict, guild_id:str):
+
+		self.guild_id:str = guild_id
+		self.exceptionrole_id:str = data.get("id", UNDEFINED)
+		self.role_id:str = data.get("role_id", UNDEFINED)
+
+	def __repr__(self):
+		return f"<{self.__class__.__name__} server='{self.guild_id}' role_id='{self.role_id}'>"
+
+	def toJSON(self) -> dict:
+		""" Returns a json save dict representation of all values for API, storage, etc... """
+
+		j:dict = dict()
+
+		j["exceptionrole_id"] = self.toString(self.exceptionrole_id)
+		j["role_id"] = self.toString(self.role_id)
+
+		return j
