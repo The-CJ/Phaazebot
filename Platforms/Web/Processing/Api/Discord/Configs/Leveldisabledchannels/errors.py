2d8494d19fee5c2fa1b47d289d42fb79d4543624
The_CJ <christoperjaekel@googlemail.com>
2020-02-26 18:48:36 +0100

    new error file for create of disabled level channel

---
+++ Platforms/Web/Processing/Api/Discord/Configs/Leveldisabledchannels/errors.py
@@ -0,0 +1,78 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Web.index import WebIndex
+
+import json
+from aiohttp.web import Response, Request
+
+async def apiDiscordConfigsLevelDisabledChannelExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+			channel_id:str
+			channel_name:str
+	"""
+	res:dict = dict(status=400, error="discord_disabled_levelchannel_exists")
+
+	channel_id:str = kwargs.get("channel_id", "")
+	if channel_id:
+		res["channel_id"] = str(channel_id)
+
+	channel_name:str = kwargs.get("channel_name", "")
+	if channel_name:
+		res["channel_name"] = str(channel_name)
+
+	# build message
+	default_msg:str = "Disabled level channel already exists"
+
+	if channel_name:
+		default_msg += f" for '{channel_name}'"
+
+	if channel_id:
+		default_msg += f" (Channel ID:{channel_id})"
+
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Role exists: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
+
+async def apiDiscordConfigsLevelDisabledChannelNotExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+		Takes from kwargs:
+			msg:str
+			exceptionrole_id:str
+			role_id:str
+	"""
+	res:dict = dict(status=400, error="discord_disabled_levelchannel_not_exists")
+
+	exceptionrole_id:str = kwargs.get("exceptionrole_id", "")
+	if exceptionrole_id:
+		res["exceptionrole_id"] = str(exceptionrole_id)
+
+	role_id:str = kwargs.get("role_id", "")
+	if role_id:
+		res["role_id"] = str(role_id)
+
+	# build message
+	default_msg:str = "No exceptionrole found"
+
+	if role_id:
+		default_msg += f" (Role ID:{role_id})"
+
+	if exceptionrole_id:
+		default_msg += f" (Exceptionrole ID:{exceptionrole_id})"
+
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Role does not exists: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
