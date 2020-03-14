ac81d3dc2c11c9d973c6937f188fcb80a82c9f46
The_CJ <christoperjaekel@googlemail.com>
2020-03-14 17:34:44 +0100

    get endpoint

---
+++ Platforms/Web/Processing/Api/Discord/Configs/Gameenabledchannels/get.py
@@ -0,0 +1,74 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Web.index import WebIndex
+	from Platforms.Discord.main_discord import PhaazebotDiscord
+
+import json
+import discord
+from aiohttp.web import Response, Request
+from Utils.Classes.webrequestcontent import WebRequestContent
+from Utils.Classes.discordwebuserinfo import DiscordWebUserInfo
+from Platforms.Discord.utils import getDiscordServerGameEnabledChannels, getDiscordServerGameEnabledChannelAmount
+from Platforms.Web.Processing.Api.errors import (
+	apiMissingAuthorisation,
+	apiMissingData
+)
+from Platforms.Web.Processing.Api.Discord.errors import (
+	apiDiscordGuildUnknown,
+	apiDiscordMemberNotFound,
+	apiDiscordMissingPermission
+)
+
+DEFAULT_LIMIT:int = 50
+MAX_LIMIT:int = 100
+
+async def apiDiscordConfigsGameEnabledChannelsGet(cls:"WebIndex", WebRequest:Request) -> Response:
+	"""
+		Default url: /api/discord/configs/gameenabledchannels/get
+	"""
+	Data:WebRequestContent = WebRequestContent(WebRequest)
+	await Data.load()
+
+	# get required stuff
+	guild_id:str = Data.getStr("guild_id", "", must_be_digit=True)
+	entry_id:str = Data.getStr("entry_id", "", must_be_digit=True)
+	channel_id:str = Data.getStr("channel_id", "", must_be_digit=True)
+	limit:int = Data.getInt("limit", DEFAULT_LIMIT, min_x=1, max_x=MAX_LIMIT)
+	offset:int = Data.getInt("offset", 0, min_x=0)
+
+	# checks
+	if not guild_id:
+		return await apiMissingData(cls, WebRequest, msg="missing or invalid 'guild_id'")
+
+	PhaazeDiscord:"PhaazebotDiscord" = cls.Web.BASE.Discord
+	Guild:discord.Guild = discord.utils.get(PhaazeDiscord.guilds, id=int(guild_id))
+	if not Guild:
+		return await apiDiscordGuildUnknown(cls, WebRequest)
+
+	# get user info
+	DiscordUser:DiscordWebUserInfo = await cls.getDiscordUserInfo(WebRequest)
+	if not DiscordUser.found:
+		return await apiMissingAuthorisation(cls, WebRequest)
+
+	# get member
+	CheckMember:discord.Member = Guild.get_member(int(DiscordUser.user_id))
+	if not CheckMember:
+		return await apiDiscordMemberNotFound(cls, WebRequest, guild_id=guild_id, user_id=DiscordUser.user_id)
+
+	# check permissions
+	if not (CheckMember.guild_permissions.administrator or CheckMember.guild_permissions.manage_guild):
+		return await apiDiscordMissingPermission(cls, WebRequest, guild_id=guild_id, user_id=DiscordUser.user_id)
+
+	channel_res:list = await getDiscordServerGameEnabledChannels(PhaazeDiscord, guild_id, entry_id=entry_id, channel_id=channel_id, limit=limit, offset=offset)
+
+	return cls.response(
+		text=json.dumps( dict(
+			result=[ Channel.toJSON() for Channel in channel_res ],
+			limit=limit,
+			offset=offset,
+			total=(await getDiscordServerGameEnabledChannelAmount(PhaazeDiscord, guild_id)),
+			status=200)
+		),
+		content_type="application/json",
+		status=200
+	)
