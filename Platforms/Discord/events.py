5eda2889ac773683d4960299e3d8de509e768519
The_CJ <christoperjaekel@googlemail.com>
2020-03-24 17:06:00 +0100

    leave message for discord member

--- Platforms/Discord/events.py
+++ Platforms/Discord/events.py
@@ -43,9 +43,29 @@ async def eventOnMemberJoin(cls:"PhaazebotDiscord", Member:discord.Member) -> No
 
 async def eventOnMemberRemove(cls:"PhaazebotDiscord", Member:discord.Member) -> None:
 
+	Settings:DiscordServerSettings = await getDiscordSeverSettings(cls, Member.guild.id)
+	link_in_name:bool = bool( ContainsLink.match(Member.name) )
+
 	# TODO: logging for member leave
 
-	# set member active, if there was a known entry
+	# send welcome message
+	if Settings.leave_chan and Settings.leave_msg and (not link_in_name):
+
+		LeaveChan:discord.TextChannel = getDiscordChannelFromString(cls, Member.guild, Settings.leave_chan, required_type="text")
+		if LeaveChan:
+			welcome_msg_vars:dict = {
+				"user-name": Member.name,
+				"user-mention": Member.mention,
+				"server-name": Member.guild.name,
+				"member-count": str(Member.guild.member_count)
+			}
+			finished_message:str = await responseFormater(cls, Settings.leave_msg, var_dict=welcome_msg_vars, enable_special=True, DiscordGuild=Member.guild)
+			try:
+				await LeaveChan.send(finished_message)
+			except Exception as E:
+				cls.BASE.Logger.warning(f"Can't send leave message: {E} {sys.exc_info()[0]}")
+
+	# set member inactive
 	cls.BASE.PhaazeDB.updateQuery(
 		table = "discord_user",
 		content = {"on_server":0},
