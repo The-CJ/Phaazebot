e3dbadf621b701489c716e8a89b9164fe916d611
The_CJ <cj@tbits.net>
2020-04-01 14:47:37 +0200

    adding event loggings for member remove / leave

--- Platforms/Discord/logging.py
+++ Platforms/Discord/logging.py
@@ -44,6 +44,33 @@ async def loggingOnMemberJoin(cls:"PhaazebotDiscord", Settings:DiscordServerSett
 	except Exception as E:
 		cls.BASE.Logger.warning(f"Can't log message: {E} {traceback.format_exc()}")
 
+async def loggingOnMemberRemove(cls:"PhaazebotDiscord", Settings:DiscordServerSettings, OldMember:discord.Member, **kwargs:dict) -> None:
+	"""
+	Sends logging message on member remove event to Settings.track_channel.
+	Requires track option `Member.remove` active
 
+	Optional keywords:
+	------------------
+	* link_in_name `bool` : (Default: False)
+	"""
+	link_in_name:bool = kwargs.get("link_in_name", False)
 
-	pass
+	if not ("Member.remove" in Settings.track_options): return # track option not active
+
+	TargetChannel:discord.TextChannel = getDiscordChannelFromString(cls, OldMember.guild, Settings.track_channel, required_type="text")
+	if not TargetChannel: return # no channel found
+
+	Emb:discord.Embed = discord.Embed(
+		description = f"User name: {OldMember.name}\nLast known nickname: {OldMember.nick}\nMention: {OldMember.mention}\nID: {OldMember.id}",
+		timestamp = datetime.datetime.now(),
+		color = EVENT_COLOR_NEGATIVE
+	)
+	Emb.set_thumbnail(url=OldMember.avatar_url or OldMember.default_avatar_url)
+	Emb.set_author(name="Log Event - [Member remove]")
+	if link_in_name:
+		Emb.add_field(":warning: Blocked public announcements", "Link in name", inline=True)
+
+	try:
+		await TargetChannel.send(embed=Emb)
+	except Exception as E:
+		cls.BASE.Logger.warning(f"Can't log message: {E} {traceback.format_exc()}")
