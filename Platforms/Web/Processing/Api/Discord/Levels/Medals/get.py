fd4e74948fa4bcb3adbf85b64916868f14aa934a
The_CJ <christoperjaekel@googlemail.com>
2020-04-26 18:32:26 +0200

    fixing get

--- Platforms/Web/Processing/Api/Discord/Levels/Medals/get.py
+++ Platforms/Web/Processing/Api/Discord/Levels/Medals/get.py
@@ -7,8 +7,12 @@ import json
 import discord
 from aiohttp.web import Response, Request
 from Utils.Classes.webrequestcontent import WebRequestContent
+from Platforms.Discord.utils import getDiscordUsersMedals, getDiscordUsersMedalAmount
 from Platforms.Web.Processing.Api.errors import apiMissingData
 from Platforms.Web.Processing.Api.Discord.errors import apiDiscordGuildUnknown
+
+DEFAULT_LIMIT:int = 50
+MAX_LIMIT:int = 100
 
 async def apiDiscordLevelsMedalsGet(cls:"WebIndex", WebRequest:Request) -> Response:
 	"""
@@ -21,13 +25,13 @@ async def apiDiscordLevelsMedalsGet(cls:"WebIndex", WebRequest:Request) -> Respo
 	guild_id:str = Data.getStr("guild_id", "", must_be_digit=True)
 	member_id:str = Data.getStr("member_id", "", must_be_digit=True)
 	name:str = Data.getStr("name", "", len_max=512)
+	name_contains:str = Data.getStr("name_contains", "", len_max=512)
 	medal_id:str = Data.getStr("medal_id", "", must_be_digit=True)
+	limit:int = Data.getInt("limit", DEFAULT_LIMIT, min_x=1, max_x=MAX_LIMIT)
+	offset:int = Data.getInt("offset", 0, min_x=0)
 
 	if not guild_id:
 		return await apiMissingData(cls, WebRequest, msg="missing or invalid 'guild_id'")
-
-	if not member_id:
-		return await apiMissingData(cls, WebRequest, msg="missing or invalid 'member_id'")
 
 	PhaazeDiscord:"PhaazebotDiscord" = cls.Web.BASE.Discord
 	Guild:discord.Guild = discord.utils.get(PhaazeDiscord.guilds, id=int(guild_id))
@@ -35,4 +39,16 @@ async def apiDiscordLevelsMedalsGet(cls:"WebIndex", WebRequest:Request) -> Respo
 		return await apiDiscordGuildUnknown(cls, WebRequest)
 
 	# get medals
-	# res_medals:list = await getDiscordUsersMedals(PhaazeDiscord)
+	res_medals:list = await getDiscordUsersMedals(PhaazeDiscord, guild_id, member_id=member_id, medal_id=medal_id, limit=limit, offset=offset, name=name, name_contains=name_contains)
+
+	return cls.response(
+		text=json.dumps( dict(
+			result=[ Medal.toJSON() for Medal in res_medals ],
+			limit=limit,
+			offset=offset,
+			total=(await getDiscordUsersMedalAmount(PhaazeDiscord, guild_id)),
+			status=200)
+		),
+		content_type="application/json",
+		status=200
+	)
