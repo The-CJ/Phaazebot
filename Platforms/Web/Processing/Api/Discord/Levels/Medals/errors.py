96d9d18b94bb88023522e20c68ad24c3ca76086d
The_CJ <christoperjaekel@googlemail.com>
2020-04-27 16:26:30 +0200

    error management for medal create

---
+++ Platforms/Web/Processing/Api/Discord/Levels/Medals/errors.py
@@ -0,0 +1,72 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Web.index import WebIndex
+
+import json
+from aiohttp.web import Response, Request
+
+async def apiDiscordUserMedalExists(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+	Optional keywords:
+	------------------
+	* msg `str` : (Default: None) * [Overwrites default]
+	* name `str` *
+
+	Default message (*gets altered by optional keywords):
+	----------------------------------------------------
+	Medal already exists
+	"""
+	res:dict = dict(status=400, error="discord_user_medal_exists")
+
+	name:str = kwargs.get("name", "")
+	if name:
+		res["name"] = name
+
+	# build message
+	default_msg:str = "Medal already exists"
+
+	if name:
+		default_msg += f" with name '{name}'"
+
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Medal exits: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
+
+async def apiDiscordUserMedalLimit(cls:"WebIndex", WebRequest:Request, **kwargs:dict) -> Response:
+	"""
+	Optional keywords:
+	------------------
+	* msg `str` : (Default: None) * [Overwrites default]
+	* limit `str` *
+
+	Default message (*gets altered by optional keywords):
+	----------------------------------------------------
+	You have hit the limit of medals for this member
+	"""
+	res:dict = dict(status=400, error="discord_user_medal_limit")
+
+	limit:str = kwargs.get("limit", "")
+	if limit:
+		res["limit"] = limit
+
+	# build message
+	default_msg:str = "You have hit the limit of medals for this member"
+
+	if limit:
+		default_msg += f" (Limit: {limit})"
+
+	msg:str = kwargs.get("msg", default_msg)
+	res["msg"] = msg
+
+	cls.Web.BASE.Logger.debug(f"(API/Discord) 400 Too many medals: {WebRequest.path}", require="api:400")
+	return cls.response(
+		text=json.dumps( res ),
+		content_type="application/json",
+		status=400
+	)
