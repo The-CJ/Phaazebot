aff6c88f3a3eebe3e73859cfbd06b3d68266827c
The_CJ <christoperjaekel@googlemail.com>
2020-05-02 16:14:56 +0200

    new values in storeclasse

--- Utils/Classes/storeclasses.py
+++ Utils/Classes/storeclasses.py
@@ -110,6 +110,10 @@ class VarsStore(object):
 		self.DISCORD_LOGIN_LINK:str = str(config.get("discord_login_link", "/discord"))
 		self.DISCORD_REDIRECT_LINK:str = str(config.get("discord_redirect_link", "localhost"))
 
+		self.TWITCH_BOT_ID:str = str(config.get("twitch_bot_id", "00000"))
+		self.TWITCH_LOGIN_LINK:str = str(config.get("twitch_login_link", "/twitch"))
+		self.TWITCH_REDIRECT_LINK:str = str(config.get("twitch_redirect_link", "localhost"))
+
 		self.LOGO_OSU:str = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Osu%21Logo_%282015%29.png/600px-Osu%21Logo_%282015%29.png"
 		self.LOGO_TWITCH:str = "https://i.redditmedia.com/za3YAsq33WcZc66FVb1cBw6mY5EibKpD_5hfLz0AbaE.jpg?w=320&s=53cf0ff252d84c5bb460b6ec0b195504"
 
