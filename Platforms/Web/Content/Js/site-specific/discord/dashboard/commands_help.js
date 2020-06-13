aea737c758d8c757caf34dc7a9123b40be028375
The_CJ <christoperjaekel@googlemail.com>
2020-06-13 18:14:03 +0200

    command help select styling

--- Platforms/Web/Content/Js/site-specific/discord/dashboard/commands_help.js
+++ Platforms/Web/Content/Js/site-specific/discord/dashboard/commands_help.js
@@ -7,10 +7,54 @@ var CommandsHelp = new (class {
   }
 
   show() {
-
+    this.load();
   }
 
   load(x={}) {
+    var CommandsHelpO = this;
+
+    $.get("/api/discord/commands/list", x)
+    .done(function (data) {
+
+      var HTMLSelect = $("[location=commands_help] [name=function]").html("");
+      HTMLSelect.append( $("<option value=''>Please select a command...</option>") );
+
+      for (var entry of data.result) {
+        var Option = $("<option></option>");
+        Option.attr("value", entry.function);
+        Option.text(entry.name);
+        HTMLSelect.append(Option);
+      }
+
+      CommandsHelpO.loadCommandInfo();
+
+    })
+    .fail(function (data) {
+      generalAPIErrorHandler( {data:data, msg:"Could not load command help"} );
+    })
+
+  }
+
+  // utils
+  loadCommandInfo(func="") {
+
+    if ( isEmpty(func) ) {
+      // loads in default values or taken from url
+      func = DynamicURL.get("commands_help[func]");
+    }
+
+    if ( isEmpty(func) ) {
+      // get value from select box
+      func = $("[location=commands_help] [name=function]").val();
+    }
+
+    if ( isEmpty(func) ) {
+      // forget it
+      return;
+    }
+
+    // reset in select, only needed if its set via preselect
+    $("[location=commands_help] [name=function]").val(func);
 
   }
 
