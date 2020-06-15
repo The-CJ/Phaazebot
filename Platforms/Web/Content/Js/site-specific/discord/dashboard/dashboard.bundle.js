9a609402a813767af42d6e446f2926b04155d842
The_CJ <cj@tbits.net>
2020-06-15 17:01:15 +0200

    tryed adding a js and maybe css bundle concept...

---
+++ Platforms/Web/Content/Js/site-specific/discord/dashboard/dashboard.bundle.js
@@ -0,0 +1,8 @@
+// This is a dynamic bundle file, if this is called via the /js/ endpoint
+// it will generate a single response containing all files listed below
+
+// To include files add lines starting with "//++ "
+// followed by the full /js/ path that would be needed to recieve it from web
+
+//++ site-specific/discord/dashboard/commands_assign.js
+//++ site-specific/discord/dashboard/commands_command.js
--- Platforms/Web/Processing/webcontent.py
+++ Platforms/Web/Processing/webcontent.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
 	from Platforms.Web.index import WebIndex
 
-import json, mimetypes
+import os, json, mimetypes
 from aiohttp.web import Request, Response
 
 CONTENTFOLDER_CSS = "Platforms/Web/Content/Css"
@@ -35,19 +35,58 @@ async def serveJs(cls:"WebIndex", WebRequest:Request) -> Response:
 		Default url: /js/*
 	"""
 	file_location:str = WebRequest.match_info.get("file", None)
-	if not file_location: return noFileDefined(cls)
+	if not file_location: return await noFileDefined(cls)
 
+	# remove all injection things
 	file_location = file_location.replace("..","").strip("/")
+
+	# not found in filesystem
+	if not os.path.isfile(f"{CONTENTFOLDER_JS}/{file_location}"):
+		return await fileNotFound(cls, file_location)
+
+	if file_location.endswith("bundle.js"):
+		# user requested a bundle
+		return await serveBundleJs(cls, WebRequest, file_location)
 
 	try:
 		file_content:bytes = open(f"{CONTENTFOLDER_JS}/{file_location}", "rb").read()
+
 	except FileNotFoundError:
+		# should not happen, since we checked before, but who cares
 		return await fileNotFound(cls, file_location)
 
 	return cls.response(
 		status=200,
 		content_type='application/javascript',
 		body=file_content
+	)
+
+async def serveBundleJs(cls:"WebIndex", WebRequest:Request, bundle_path:str) -> Response:
+	"""
+		Default url: /js/*.bundle.js
+		TODO: REWORK THIS
+	"""
+	if not bundle_path: return await noFileDefined(cls)
+
+	# this will be returned
+	bundle_data:bytes = b''
+
+	try:
+		bundle_info:str = open(f"{CONTENTFOLDER_JS}/{bundle_path}", "r").read()
+
+		line:str
+		for line in bundle_info.splitlines():
+			if line.startswith("//++"):
+				line = line.strip("//++").strip(' ')
+				bundle_data += open(f"{CONTENTFOLDER_JS}/{line}", "rb").read()
+
+	except FileNotFoundError:
+		return await fileNotFound(cls, line)
+
+	return cls.response(
+		status=200,
+		content_type='application/javascript',
+		body=bundle_data
 	)
 
 async def serveImg(cls:"WebIndex", WebRequest:Request) -> Response:
