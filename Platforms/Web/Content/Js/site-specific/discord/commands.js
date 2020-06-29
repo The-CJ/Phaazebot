b11c71f47fb4f6def7c51412d6c700129977546a
The_CJ <christoperjaekel@googlemail.com>
2020-06-29 22:05:23 +0200

    replaceing all command functions with a publiccommand class

--- Platforms/Web/Content/Js/site-specific/discord/commands.js
+++ Platforms/Web/Content/Js/site-specific/discord/commands.js
@@ -1,64 +1,138 @@
 $("document").ready(function () {
-  loadCommands();
+  PublicCommands.show();
 })
 
-function loadCommands() {
+var PublicCommands = new (class {
+  constructor() {
+    this.modal_id = "#command_modal";
+    this.list_id = "#command_list";
+    this.total_amount_field = "#command_amount";
+    this.phantom_class = ".command";
 
-  var guild_id = $("#guild_id").val();
+    this.default_limit = 50;
+    this.default_page = 0;
 
-  $.get("/api/discord/commands/get", {guild_id: guild_id})
-  .done(function (data) {
+    this.current_limit = 0;
+    this.current_page = 0;
+    this.current_max_page = 0;
+  }
 
-    $("#command_amount").text(data.result.length);
+  show() {
+    // loads in default values or taken from url
+    let limit = DynamicURL.get("limit") || this.default_limit;
+    let page = DynamicURL.get("page") || this.default_page;
 
-    for (command of data.result) {
-      var Template = $("[phantom] .command").clone();
-      Template.find(".trigger").text(command.trigger);
-      Template.find(".function").text(command.name ? command.name : "");
-      Template.find(".require").text( discordTranslateRequire(command.require) );
-      Template.find(".cost").text(command.cost);
-      Template.find(".uses").text(command.uses);
-      Template.attr("command-id", command.command_id);
+    var req = {
+      limit: limit,
+      offset: (page * limit),
+    };
 
-      if (command.hidden) {
-        Template.find(".function").addClass("hidden");
-        Template.find(".function").attr("title", "Execute the command in the Discord server to see the result.");
+    this.load(req);
+  }
+
+  load(x={}) {
+    var PublicCommandsO = this;
+    x["guild_id"] = $("#guild_id").val();
+
+    $.get("/api/discord/commands/get", x)
+    .done(function (data) {
+
+      // update view
+      PublicCommandsO.updatePageIndexButtons(data);
+
+      $(PublicCommandsO.total_amount_field).text(data.total);
+      var EntryList = $(PublicCommandsO.list_id).html("");
+
+      for (var entry of data.result) {
+        var Template = $(`[phantom] ${PublicCommandsO.phantom_class}`).clone();
+
+        entry.require = discordTranslateRequire(entry.require);
+        insertData(Template, entry);
+        Template.attr("command-id", entry.command_id);
+
+        if (entry.hidden) {
+          // TODO: add hidden
+          Template.find("[name=name]").addClass("hidden");
+          Template.find("[name=name]").attr("title", "Execute the command in the Discord server to see the result.");
+        }
+        EntryList.append(Template);
       }
 
-      $("#command_list").append(Template);
+    })
+    .fail(function (data) {
+      generalAPIErrorHandler( {data:data, msg:"could not load commands"} );
+    });
+  }
+
+  // utils
+  nextPage(last=false) {
+    this.current_page += 1;
+
+    var search = extractData("main form.controlls");
+    search["offset"] = (this.current_page * search["limit"]);
+    this.load(search);a
+  }
+
+  prevPage(first=false) {
+    this.current_page -= 1;
+    if (first) { this.current_page = 0; }
+
+    var search = extractData("main form.controlls");
+    search["offset"] = (this.current_page * search["limit"]);
+    this.load(search);
+  }
+
+  updatePageIndexButtons(data) {
+    this.current_limit = data.limit;
+    this.current_page = data.offset / data.limit;
+    this.current_max_page = (data.total / data.limit);
+    this.current_max_page = parseInt(this.current_max_page)
+
+    // update limit url if needed
+    if (this.current_limit != this.default_limit) {
+      DynamicURL.set("limit", this.current_limit);
+    } else {
+      DynamicURL.set("limit", null);
     }
 
-  })
-  .fail(function (data) {
-    generalAPIErrorHandler( {data:data, msg:"could not load commands"} );
-  })
+    // update page url if needed
+    if (this.current_page != this.default_page) {
+      DynamicURL.set("page", this.current_page);
+    } else {
+      DynamicURL.set("page", null);
+    }
 
-}
+    // update html elements
+    $("main form.controlls [name=limit]").val(this.current_limit);
+    $("main form.controlls .pages .prev").attr("disabled", (this.current_page <= 0) );
+    $("main form.controlls .pages .next").attr("disabled", (this.current_page >= this.current_max_page) );
+    $("main form.controlls .pages .page").text(this.current_page+1);
+  }
 
-function detailCommand(HTMLCommand) {
+  // edit
+  editModal(HTMLButton) {
+    var PublicCommandsO = this;
+    var req = {
+      "guild_id": $("#guild_id").val(),
+      "command_id": $(HTMLButton).closest(PublicCommandsO.phantom_class).attr("command-id")
+    };
 
-  HTMLCommand = $(HTMLCommand);
+    $.get("/api/discord/commands/get", req)
+    .done(function (data) {
+      var command = data.result.pop();
 
-  var guild_id = $("#guild_id").val();
-  var command_id = HTMLCommand.attr("command-id");
+      let curr_single = $("#guild_currency").val();
+      let curr_multi = $("#guild_currency_multi").val();
+      command.cost = command.cost + ' ' + (command.cost == 1 ? curr_single : curr_multi);
+      command.uses = `${command.uses} times`;
+      command.require = discordTranslateRequire(command.require);
+      insertData(PublicCommandsO.modal_id, command);
 
-  $.get("/api/discord/commands/get", {guild_id: guild_id, command_id:command_id})
-  .done(function (data) {
-    var command = data.result[0];
-    var currency = command.cost == 1 ? $("#guild_currency").val() : $("#guild_currency_multi").val();
-    console.log(command);
-    $("#command_detail [name=trigger]").text(command.trigger);
-    $("#command_detail [name=require]").text( discordTranslateRequire(command.require) );
-    $("#command_detail [name=uses]").text( command.uses + " times" );
-    $("#command_detail [name=cost]").text( command.cost + " " + currency );
-    $("#command_detail [name=name]").text( command.name ? command.name : "(Hidden Command)" );
-    $("#command_detail [name=description]").text( command.description ? command.description : "Execute the command in the Discord server to see the result." );
+      $(PublicCommandsO.modal_id).modal("show");
+    })
+    .fail(function (data) {
+      generalAPIErrorHandler( {data:data, msg:"could not load command detail"} );
+    });
+  }
 
-
-    $("#command_detail").modal("show");
-  })
-  .fail(function (data) {
-    generalAPIErrorHandler( {data:data, msg:"could not load command detail"} );
-  })
-
-}
+});
