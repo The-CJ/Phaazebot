42d833072b38568c874960bb8cd6e4eff5f9ea1c
The_CJ <christoperjaekel@googlemail.com>
2020-07-18 22:01:36 +0200

    added base files in html and js for regulars

---
+++ Platforms/Web/Content/Js/site-specific/discord/dashboard/regular.js
@@ -0,0 +1,153 @@
+var Regulars = new(class {
+  constructor() {
+    this.modal_id = "#regular_modal";
+    this.list_id = "#regular_list";
+    this.total_field_id = "#regular_amount";
+    this.phantom_class = ".regular";
+
+    this.default_limit = 50;
+    this.default_page = 0;
+
+    this.current_limit = 0;
+    this.current_page = 0;
+    this.current_max_page = 0;
+  }
+
+  show() {
+    // loads in default values or taken from url
+    let limit = DynamicURL.get("regulars[limit]") || this.default_limit;
+    let page = DynamicURL.get("regulars[page]") || this.default_page;
+
+    var req = {
+      limit: limit,
+      offset: (page * limit)
+    };
+
+    this.load( req );
+  }
+
+  load(x={}) {
+    var RegularO = this;
+    x["guild_id"] = $("#guild_id").val();
+    x["detailed"] = true;
+
+    $.get("/api/discord/regulars/get", x)
+    .done(function (data) {
+
+      // update view
+      RegularO.updatePageIndexButtons(data);
+
+      var EntryList = $(RegularO.list_id).html("");
+      $(RegularO.total_field_id).text(data.total);
+
+      for (var entry of data.result) {
+        var Template = $(`[phantom] ${RegularO.phantom_class}`).clone();
+
+        // set avatar
+        var avatar = discordUserAvatar(level.member_id, level.avatar);
+        Template.find("img").attr("src", avatar);
+
+        insertData(Template, entry);
+        Template.attr("regular-id", entry.regular_id);
+
+        EntryList.append(Template);
+      }
+
+    })
+    .fail(function (data) {
+      generalAPIErrorHandler( {data:data, msg:"could not load regulars"} );
+    })
+  }
+
+  // utils
+  nextPage(last=false) {
+    this.current_page += 1;
+    if (last) { this.current_page = this.current_max_page; }
+
+    var search = extractData("[location=regulars] .controls");
+    search["offset"] = (this.current_page * search["limit"]);
+    this.load(search);
+  }
+
+  prevPage(first=false) {
+    this.current_page -= 1;
+    if (first) { this.current_page = 0; }
+
+    var search = extractData("[location=regulars] .controls");
+    search["offset"] = (this.current_page * search["limit"]);
+    this.load(search);
+  }
+
+  updatePageIndexButtons(data) {
+    this.current_limit = data.limit;
+    this.current_page = data.offset / data.limit;
+    this.current_max_page = (data.total / data.limit);
+    this.current_max_page = Math.ceil(this.current_max_page - 1);
+
+    // update limit url if needed
+    if (this.current_limit != this.default_limit) {
+      DynamicURL.set("regulars[limit]", this.current_limit);
+    } else {
+      DynamicURL.set("regulars[limit]", null);
+    }
+
+    // update page url if needed
+    if (this.current_page != this.default_page) {
+      DynamicURL.set("regulars[page]", this.current_page);
+    } else {
+      DynamicURL.set("regulars[page]", null);
+    }
+
+    // update html elements
+    $("[location=regulars] [name=limit]").val(this.current_limit);
+    $("[location=regulars] .pages .prev").attr("disabled", (this.current_page <= 0) );
+    $("[location=regulars] .pages .next").attr("disabled", (this.current_page >= this.current_max_page) );
+    $("[location=regulars] .pages .page").text(this.current_page+1);
+  }
+
+  // edit
+  editModal(HTMLButton) {
+    var LevelO = this;
+
+    var req = {
+      "detailed": true,
+      "guild_id": $("#guild_id").val(),
+      "member_id": $(HTMLButton).closest(LevelO.phantom_class).attr("member-id")
+    };
+    $.get("/api/discord/levels/get", req)
+    .done(function (data) {
+
+      var level = data.result.pop();
+
+      // extra values
+      level.display_rank = (level.rank ? `Rank: #${level.rank}` : "Rank: [N/A]");
+      level.display_id = (level.member_id ? `ID: ${level.member_id}` : "ID: [N/A]");
+
+      // set avatar
+      var avatar = discordUserAvatar(level.member_id, level.avatar, 128);
+      $(`${LevelO.modal_id} img`).attr("src", avatar);
+
+      // if name is [N/A], that could mean the user is not on the server, but phaaze did not catch the event to remove him,
+      // in this case the owner has the option to remove this user
+      // NOTE: if member name themself "[N/A]" there can be removed even duh there are on the server.
+      //       that is a wanted feature since a owner can do this at any time, via a API request
+      //       So kids, dont be dumb and call yourself '[N/A]' and delete your avatar or you may get deleted
+      if (level["username"] == "[N/A]" && level["avatar"] == null) {
+        $(`${LevelO.modal_id} button[name=on_server]`).show();
+      } else {
+        $(`${LevelO.modal_id} button[name=on_server]`).hide();
+      }
+
+      insertData(LevelO.modal_id, level);
+      LevelO.loadinModalMedal(level.guild_id, level.member_id);
+      $(LevelO.modal_id).modal("show");
+
+    })
+    .fail(function (data) {
+      generalAPIErrorHandler( {data:data, msg:"Could not load level details"} );
+    });
+  }
+
+  // delete
+
+});
