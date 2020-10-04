2885a9d760cede97e04d25bc21a553a5b2270300
The_CJ <christoperjaekel@googlemail.com>
2020-10-04 16:54:49 +0200

    added new object and db functions for twitch user stats

--- Platforms/Twitch/db.py
+++ Platforms/Twitch/db.py
@@ -5,8 +5,9 @@ if TYPE_CHECKING:
 import twitch_irc
 
 from Utils.Classes.twitchchannelsettings import TwitchChannelSettings
+from Utils.Classes.twitchuserstats import TwitchUserStats
 
-# twitch settings
+# twitch_settings
 async def getTwitchChannelSettings(cls:"PhaazebotTwitch", origin:twitch_irc.Message or str or int, prevent_new:bool=False) -> TwitchChannelSettings:
 	"""
 	Get channel settings for a twitch channel
@@ -58,3 +59,92 @@ async def makeTwitchChannelSettings(cls:"PhaazebotTwitch", channel_id:str) -> Tw
 	except:
 		cls.BASE.Logger.critical(f"(Twitch) New channel settings failed: {channel_id=}")
 		raise RuntimeError("Creating new DB entry failed")
+
+# twitch_user
+async def getTwitchChannelUsers(cls:"PhaazebotTwitch", channel_id:str, **search:dict) -> List[TwitchUserStats]:
+	"""
+	Get channel levels and stats.
+	Returns a list of TwitchUserStats().
+
+	Optional keywords:
+	------------------
+	* user_id `str` : (Default: None)
+	* edited `int`: (Default: 0) [0=all, 1=not edited, 2=only edited]
+	* name `str`: (Default: None)
+	* name_contains `str`: (Default: None) [DB uses LIKE]
+	* order_str `str`: (Default: "ORDER BY id")
+	* limit `int`: (Default: None)
+	* offset `int`: (Default: 0)
+	"""
+	# unpack
+	user_id:str = search.get("user_id", None)
+	edited:int = search.get("edited", 0)
+	name:str = search.get("name", None)
+	name_contains:str = search.get("name_contains", None)
+	order_str:str = search.get("order_str", "ORDER BY `id`")
+	limit:int = search.get("limit", None)
+	offset:int = search.get("offset", 0)
+
+	# process
+	sql:str = """
+		WITH `twitch_user` AS (
+			SELECT
+				`twitch_user`.*,
+				`twitch_user_name`.`user_display_name` AS `display_name`,
+				`twitch_user_name`.`user_name` AS `name`,
+				RANK() OVER (ORDER BY `amount_time` DESC) AS `rank`
+			FROM `twitch_user`
+			LEFT JOIN `twitch_user_name`
+				ON `twitch_user`.`user_id` = `twitch_user_name`.`user_id`
+			WHERE `twitch_user`.`channel_id` = %s
+			GROUP BY `twitch_user`.`channel_id`, `twitch_user`.`user_id`
+		)
+		SELECT `twitch_user`.* FROM `twitch_user` WHERE 1=1"""
+
+	values:tuple = ( str(channel_id), )
+
+	if user_id:
+		sql += " AND `twitch_user`.`user_id` = %s"
+		values += ( str(user_id), )
+
+	if name:
+		sql += " AND (`twitch_user`.`name` = %s OR `twitch_user`.`display_name` = %s)"
+		values += ( str(name), str(name) )
+
+	if name_contains:
+		name_contains = f"%{name_contains}%"
+		sql += " AND (`twitch_user`.`name` LIKE %s OR `twitch_user`.`display_name` LIKE %s)"
+		values += ( str(name_contains), str(name_contains) )
+
+	if edited == 2:
+		sql += " AND `twitch_user`.`edited` = 1"
+	if edited == 1:
+		sql += " AND `twitch_user`.`edited` = 0"
+
+	sql += f" {order_str}"
+
+	if limit:
+		sql += f" LIMIT {limit}"
+		if offset:
+			sql += f" OFFSET {offset}"
+
+	print(sql)
+	res:List[dict] = cls.BASE.PhaazeDB.selectQuery(sql, values)
+
+	if res:
+		return [TwitchUserStats(x) for x in res]
+
+	else:
+		return []
+
+async def getTwitchChannelUserAmount(cls:"PhaazebotTwitch", channel_id:str, where:str="1=1", where_values:tuple=()) -> int:
+
+	sql:str = f"""
+		SELECT COUNT(*) AS `I` FROM `twitch_user`
+		WHERE `twitch_user`.`channel_id` = %s AND {where}"""
+
+	values:tuple = (channel_id,) + where_values
+
+	res:List[dict] = cls.BASE.PhaazeDB.selectQuery(sql, values)
+
+	return res[0]["I"]
---
+++ Utils/Classes/twitchuserstats.py
@@ -0,0 +1,69 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from Platforms.Twitch.main_twitch import PhaazebotTwitch
+
+from Utils.Classes.undefined import UNDEFINED
+from Utils.Classes.dbcontentclass import DBContentClass
+from Utils.Classes.apiclass import APIClass
+
+class TwitchUserStats(DBContentClass, APIClass):
+	"""
+	Contains and represents all phaaze values for a Twitch user
+	"""
+	def __init__(self, data:dict):
+
+		# key
+		self.channel_id:str = data.get("channel_id", UNDEFINED)
+		self.user_id:str = data.get("user_id", UNDEFINED)
+
+		# vars
+		self.active:int = data.get("active", UNDEFINED)
+		self.amount_currency:int = int( data.get("exp", 0) )
+		self.amount_time:int = int( data.get("currency", 0) )
+
+		# calc
+		self.rank:int = data.get("rank", UNDEFINED)
+		self.edited:bool = bool( data.get("edited", False) )
+		self.regular:bool = bool( data.get("regular", False) )
+
+	def __repr__(self):
+		return f"<{self.__class__.__name__} channel='{self.channel_id}' user='{self.user_id}'>"
+
+	async def editCurrency(self, cls:"PhaazebotTwitch", amount_by:int=None, amount_to:int=None) -> None:
+
+		if amount_by and amount_to: raise AttributeError("'amount_by' and 'amount_to' can't both be given")
+
+		sql:str = "UPDATE `twitch_user`"
+		values:tuple = ()
+
+		if amount_by != None:
+			sql += " SET `amount_currency` = `amount_currency` + %s"
+			values += ( int(amount_by) )
+
+		if amount_to != None:
+			sql += " SET `amount_currency` = %s"
+			values += ( int(amount_to) )
+
+		sql += " WHERE `channel_id` = %s AND `user_id` = %s"
+		values += ( str(self.channel_id), str(self.user_id) )
+
+		cls.BASE.PhaazeDB.query(sql, values)
+		cls.BASE.Logger.debug(f"(Twitch) Updated currency: {self.channel_id=} {self.user_id=}", require="twitch:level")
+
+	def toJSON(self, include_active:bool=False) -> dict:
+		""" Returns a json save dict representation of all values for API, storage, etc... """
+
+		j:dict = dict()
+
+		j["channel_id"] = self.toString(self.channel_id)
+		j["user_id"] = self.toString(self.user_id)
+		j["amount_currency"] = self.toInteger(self.amount_currency)
+		j["amount_time"] = self.toInteger(self.amount_time)
+		j["rank"] = self.toList(self.rank)
+		j["edited"] = self.toBoolean(self.edited)
+		j["regular"] = self.toBoolean(self.regular)
+
+		if include_active:
+			j["active"] = self.toInteger(self.active)
+
+		return j
