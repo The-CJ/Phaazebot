0031991d46411543120c462882191614d73ca011
The_CJ <christoperjaekel@googlemail.com>
2020-10-31 20:55:55 +0100

    deleted old stuff from commands index

--- Platforms/Twitch/commandindex.py
+++ Platforms/Twitch/commandindex.py
@@ -1,16 +1,8 @@
 from typing import Awaitable
 from Utils.Classes.storeclasses import GlobalStorage
+import Platforms.Twitch.const as TwitchConst
 
 from .Processing.textonly import textOnly
-
-REQUIRE_EVERYONE:int = 0
-REQUIRE_SUB:int = 1
-REQUIRE_VIP:int = 2
-REQUIRE_REGULAR:int = 3
-REQUIRE_MOD:int = 4
-REQUIRE_OWNER:int = 5
-REQUIRE_ADMIN:int = 6
-REQUIRE_SYSTEM:int = 7
 
 command_register:list = [
 	dict(
@@ -24,7 +16,7 @@ command_register:list = [
 		need_content = True,
 		allowes_content = True,
 		example_calls = ["!myCommand", ">show_Something", "-text-dummy"],
-		recommended_require = REQUIRE_EVERYONE,
+		recommended_require = TwitchConst.REQUIRE_EVERYONE,
 		recommended_cooldown = 10,
 	),
 ]
