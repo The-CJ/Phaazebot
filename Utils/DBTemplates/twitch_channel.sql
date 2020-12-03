b1535f261bdb0ffc6aa016497a65b928f143efe5
The_CJ <christoperjaekel@googlemail.com>
2020-12-03 18:28:39 +0100

    added new field for last state change

--- Utils/DBTemplates/twitch_channel.sql
+++ Utils/DBTemplates/twitch_channel.sql
@@ -1,15 +1,16 @@
 /*
 DESCRIBE `twitch_channel`;
 
-+------------+--------------+------+-----+---------+----------------+
-| Field      | Type         | Null | Key | Default | Extra          |
-+------------+--------------+------+-----+---------+----------------+
-| id         | int(11)      | NO   | PRI | NULL    | auto_increment |
-| channel_id | varchar(128) | NO   | UNI | NULL    |                |
-| game_id    | varchar(128) | YES  |     | NULL    |                |
-| live       | tinyint(1)   | YES  |     | 0       |                |
-| managed    | tinyint(1)   | YES  |     | 0       |                |
-+------------+--------------+------+-----+---------+----------------+
++----------------------+--------------+------+-----+---------------------+----------------+
+| Field                | Type         | Null | Key | Default             | Extra          |
++----------------------+--------------+------+-----+---------------------+----------------+
+| id                   | int(11)      | NO   | PRI | NULL                | auto_increment |
+| channel_id           | varchar(128) | NO   | UNI | NULL                |                |
+| game_id              | varchar(128) | YES  |     | NULL                |                |
+| live                 | tinyint(1)   | YES  |     | 0                   |                |
+| managed              | tinyint(1)   | YES  |     | 0                   |                |
+| last_state_change_at | datetime     | YES  |     | 2000-01-01 00:00:00 |                |
++----------------------+--------------+------+-----+---------------------+----------------+
 */
 
 -- SHOW CREATE TABLE `twitch_channel`;
@@ -20,6 +21,7 @@ CREATE TABLE `twitch_channel` (
   `game_id` varchar(128) DEFAULT NULL,
   `live` tinyint(1) DEFAULT 0,
   `managed` tinyint(1) DEFAULT 0,
+  `last_state_change_at` datetime DEFAULT '2000-01-01 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `channel_id` (`channel_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
