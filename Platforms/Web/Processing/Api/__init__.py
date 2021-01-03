074cdc2e0acd2c74471f73a927169a5fd97185d1
The_CJ <christoperjaekel@googlemail.com>
2021-01-03 18:59:52 +0100

    tree down load for web proccessing

---
+++ Platforms/Web/Processing/Api/__init__.py
@@ -0,0 +1,6 @@
+# Account
+# Admin
+# Discord
+# Twitch
+
+import Platforms.Web.Processing.Api.errors as errors
---
+++ Platforms/Web/Processing/__init__.py
@@ -0,0 +1,8 @@
+# Account
+# Admin
+import Platforms.Web.Processing.Api as Api
+# Discord
+
+import Platforms.Web.Processing.errors as errors
+import Platforms.Web.Processing.mainsite as mainsite
+import Platforms.Web.Processing.webcontent as webcontent
