b5decfcde329531c7b918a496b76cbb9ccbdd960
The_CJ <christoperjaekel@googlemail.com>
2021-01-04 01:22:41 +0100

    i did a opsi

--- Platforms/Web/Processing/Api/Account/logout.py
+++ Platforms/Web/Processing/Api/Account/logout.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-	from Platforms.Web.main_web import PhaazeWebIndex
+	from Platforms.Web.main_web import PhaazebotWeb
 
 import json
 from aiohttp.web import Response
@@ -11,7 +11,7 @@ from Utils.Classes.extendedrequest import ExtendedRequest
 from Platforms.Web.Processing.Api.errors import apiUserNotFound
 from Platforms.Web.utils import authWebUser
 
-async def apiAccountLogoutPhaaze(cls:"PhaazeWebIndex", WebRequest:ExtendedRequest) -> Response:
+async def apiAccountLogoutPhaaze(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
 	Default url: /api/account/phaaze/logout
 	"""
@@ -20,13 +20,13 @@ async def apiAccountLogoutPhaaze(cls:"PhaazeWebIndex", WebRequest:ExtendedReques
 	if not AuthWeb.found:
 		return await apiUserNotFound(cls, WebRequest, msg="Not logged in")
 
-	cls.Web.BASE.PhaazeDB.deleteQuery("""
+	cls.BASE.PhaazeDB.deleteQuery("""
 		DELETE FROM `session_phaaze`
 		WHERE `session_phaaze`.`user_id` = %s""",
 		(AuthWeb.User.user_id,)
 	)
 
-	cls.Web.BASE.Logger.debug(f"(API) Logout - User: {AuthWeb.User.username}", require="api:logout")
+	cls.BASE.Logger.debug(f"(API) Logout - User: {AuthWeb.User.username}", require="api:logout")
 	return cls.response(
 		text=json.dumps(dict(status=200)),
 		content_type="application/json",
@@ -34,7 +34,7 @@ async def apiAccountLogoutPhaaze(cls:"PhaazeWebIndex", WebRequest:ExtendedReques
 	)
 
 # TODO: rework
-async def apiAccountLogoutDiscord(cls:"PhaazeWebIndex", WebRequest:ExtendedRequest) -> Response:
+async def apiAccountLogoutDiscord(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
 	Default url: /api/account/discord/logout
 	"""
@@ -59,7 +59,7 @@ async def apiAccountLogoutDiscord(cls:"PhaazeWebIndex", WebRequest:ExtendedReque
 	)
 
 # TODO: rework
-async def apiAccountLogoutTwitch(cls:"PhaazeWebIndex", WebRequest:ExtendedRequest) -> Response:
+async def apiAccountLogoutTwitch(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
 	Default url: /api/account/twitch/logout
 	"""
