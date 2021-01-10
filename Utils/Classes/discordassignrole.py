bc3ac2e3a9f1a16b5bf1f8fa1b247689576a2036
The_CJ <christoperjaekel@googlemail.com>
2021-01-10 21:59:36 +0100

    more class rewrites

--- Utils/Classes/discordassignrole.py
+++ Utils/Classes/discordassignrole.py
@@ -1,8 +1,7 @@
 from Utils.Classes.undefined import UNDEFINED
-from Utils.Classes.dbcontentclass import DBContentClass
-from Utils.Classes.apiclass import APIClass
+from Utils.Classes.contentclass import ContentClass
 
-class DiscordAssignRole(DBContentClass, APIClass):
+class DiscordAssignRole(ContentClass):
 	"""
 	Contains and represents stuff for a discord assign role
 	"""
@@ -12,20 +11,20 @@ class DiscordAssignRole(DBContentClass, APIClass):
 	def __init__(self, data:dict):
 
 		# key
-		self.assignrole_id:int = data.get("id", UNDEFINED)
-		self.guild_id:str = data.get("guild_id", UNDEFINED)
+		self.assignrole_id:int = self.asInteger(data.get("id", UNDEFINED))
+		self.guild_id:str = self.asString(data.get("guild_id", UNDEFINED))
 
 		# vars
-		self.role_id:str = data.get("role_id", UNDEFINED)
-		self.trigger:str = data.get("trigger", UNDEFINED)
+		self.role_id:str = self.asString(data.get("role_id", UNDEFINED))
+		self.trigger:str = self.asString(data.get("trigger", UNDEFINED))
 
 	def toJSON(self) -> dict:
 		""" Returns a json save dict representation of all values for API, storage, etc... """
 
 		j:dict = dict()
 
-		j["assignrole_id"] = self.toString(self.assignrole_id)
-		j["role_id"] = self.toString(self.role_id)
-		j["trigger"] = self.toString(self.trigger)
+		j["assignrole_id"] = self.asString(self.assignrole_id)
+		j["role_id"] = self.asString(self.role_id)
+		j["trigger"] = self.asString(self.trigger)
 
 		return j
