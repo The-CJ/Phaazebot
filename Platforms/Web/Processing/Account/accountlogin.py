bb522ffc51c55933285ca6ec1b6e24b96a1cfd43
The_CJ <christoperjaekel@googlemail.com>
2021-01-24 21:36:34 +0100

    reworked web: accountLogin, accountCreate

--- Platforms/Web/Processing/Account/accountlogin.py
+++ Platforms/Web/Processing/Account/accountlogin.py
@@ -1,27 +1,31 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-	from Platforms.Web.index import WebIndex
+	from Platforms.Web.main_web import PhaazebotWeb
 
-from aiohttp.web import Response, Request
+from aiohttp.web import Response
 from Utils.Classes.htmlformatter import HTMLFormatter
+from Utils.Classes.extendedrequest import ExtendedRequest
 from Utils.Classes.authwebuser import AuthWebUser
-from Platforms.Web.utils import getNavbar
+from Platforms.Web.index import PhaazeWebIndex
+from Platforms.Web.utils import getNavbar, authWebUser
+from Platforms.Web.Processing.Account.accountmain import accountMain
 
-async def accountLogin(cls:"WebIndex", WebRequest:Request) -> Response:
+@PhaazeWebIndex.get("/account/login")
+async def accountLogin(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
-		Default url: /account/login
+	Default url: /account/login
 	"""
-	WebUser:AuthWebUser = await cls.getWebUserInfo(WebRequest)
-	if WebUser.found: return await cls.accountMain(WebRequest)
+	WebUser:AuthWebUser = await authWebUser(cls, WebRequest)
+	if WebUser.found: return await accountMain(cls, WebRequest)
 
 	AccountLogin:HTMLFormatter = HTMLFormatter("Platforms/Web/Content/Html/Account/login.html")
 
 	site:str = cls.HTMLRoot.replace(
-		replace_empty = True,
+		replace_empty=True,
 
-		title = "Phaaze | Account - Login",
-		header = getNavbar(),
-		main = AccountLogin
+		title="Phaaze | Account - Login",
+		header=getNavbar(),
+		main=AccountLogin
 	)
 
 	return cls.response(
--- Platforms/Web/Processing/__init__.py
+++ Platforms/Web/Processing/__init__.py
@@ -1,6 +1,6 @@
 import Platforms.Web.Processing.Account as Account
 import Platforms.Web.Processing.Admin as Admin
-import Platforms.Web.Processing.Api as Api
+# import Platforms.Web.Processing.Api as Api
 import Platforms.Web.Processing.Discord as Discord
 
 import Platforms.Web.Processing.errors as errors
