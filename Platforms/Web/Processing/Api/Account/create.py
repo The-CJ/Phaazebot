b17b0af3f724d64e0b903e37a5922daa49f6d460
The_CJ <christoperjaekel@googlemail.com>
2021-01-24 23:03:07 +0100

    reworked edit api function for account create

--- Platforms/Web/Processing/Api/Account/create.py
+++ Platforms/Web/Processing/Api/Account/create.py
@@ -1,32 +1,27 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-	from Platforms.Web.index import WebIndex
+	from Platforms.Web.main_web import PhaazebotWeb
 
 import json
 import re
-from aiohttp.web import Response, Request
+from aiohttp.web import Response
 from Utils.Classes.authwebuser import AuthWebUser
+from Utils.Classes.extendedrequest import ExtendedRequest
 from Utils.Classes.webrequestcontent import WebRequestContent
 from Platforms.Web.db import getWebUsers
+from Platforms.Web.utils import authWebUser
 from Utils.regex import IsEmail
-from Utils.stringutils import passwordToHash as password_function
-from Platforms.Web.Processing.Api.Account.errors import (
-	apiAccountAlreadyLoggedIn,
-	apiAccountPasswordsDontMatch,
-	apiAccountPasswordToShort,
-	apiAccountEmailWrong,
-	apiAccountTaken
-)
+from Utils.stringutils import passwordToHash as passwordFunction
 
-async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response:
+async def apiAccountCreatePhaaze(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
 		Default url: /api/account/phaaze/create
 	"""
 
-	WebUser:AuthWebUser = await cls.getWebUserInfo(WebRequest)
+	WebAuth:AuthWebUser = await authWebUser(cls, WebRequest)
 
-	if WebUser.found:
-		return await apiAccountAlreadyLoggedIn(cls, WebRequest, user_id=WebUser.user_id)
+	if WebAuth.found:
+		return await cls.Tree.Api.Account.errors.apiAccountAlreadyLoggedIn(cls, WebRequest)
 
 	Data:WebRequestContent = WebRequestContent(WebRequest)
 	await Data.load()
@@ -39,31 +34,31 @@ async def apiAccountCreatePhaaze(cls:"WebIndex", WebRequest:Request) -> Response
 
 	# checks
 	if password != password2:
-		return await apiAccountPasswordsDontMatch(cls, WebRequest)
+		return await cls.Tree.Api.Account.errors.apiAccountPasswordsDontMatch(cls, WebRequest)
 
 	if len(password) < 8:
-		return await apiAccountPasswordToShort(cls, WebRequest, min_length=8)
+		return await cls.Tree.Api.Account.errors.apiAccountPasswordToShort(cls, WebRequest, min_length=8)
 
 	if not re.match(IsEmail, email):
-		return await apiAccountEmailWrong(cls, WebRequest, email=email)
+		return await cls.Tree.Api.Account.errors.apiAccountEmailWrong(cls, WebRequest, email=email)
 
-	res_users:list = await getWebUsers( cls, where="LOWER(`user`.`username`) = LOWER(%s) OR LOWER(`user`.`email`) = LOWER(%s)",	where_values=(username, email) )
+	res_users:list = await getWebUsers(cls, overwrite_where=" AND LOWER(`user`.`username`) = LOWER(%s) OR LOWER(`user`.`email`) = LOWER(%s)", overwrite_where_values=(username, email))
 	if res_users:
-		return await apiAccountTaken(cls, WebRequest, email=email, username=username)
+		return await cls.Tree.Api.Account.errors.apiAccountTaken(cls, WebRequest, email=email, username=username)
 
 	# everything ok -> create
-	user_id:int = cls.Web.BASE.PhaazeDB.insertQuery(
-		table = "user",
-		content = {
+	user_id:int = cls.BASE.PhaazeDB.insertQuery(
+		table="user",
+		content={
 			"username": username,
-			"password": password_function(password),
+			"password": passwordFunction(password),
 			"email": email,
 		}
 	)
 
-	cls.Web.BASE.Logger.debug(f"(API) Account: Created {user_id=}", require="api:create")
+	cls.BASE.Logger.debug(f"(API) Account: Created {user_id=}", require="api:create")
 	return cls.response(
-		body=json.dumps( dict(status=200, message="successfull created user", user_id=user_id, username=username) ),
+		body=json.dumps(dict(status=200, message="successful created user", user_id=user_id, username=username)),
 		content_type="application/json",
 		status=200
 	)
