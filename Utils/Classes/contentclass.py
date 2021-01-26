b77dbc429e5eaa957561d062261f853b68cb88e7
The_CJ <christoperjaekel@googlemail.com>
2021-01-26 12:02:27 +0100

    added allow null to all contentclass functions

--- Utils/Classes/contentclass.py
+++ Utils/Classes/contentclass.py
@@ -1,7 +1,8 @@
-from typing import Any, Union, List
+from typing import Any, Union, List, Optional
 
 import json
 from datetime import datetime
+from decimal import Decimal
 from Utils.Classes.undefined import Undefined
 
 class ContentClass(object):
@@ -18,10 +19,13 @@ class ContentClass(object):
 		raise AttributeError(f"{self.__class__.__name__} missing a .toJSON()")
 
 	@staticmethod
-	def asString(data:Any) -> str:
+	def asString(data:Any, allow_null:bool = False) -> Optional[str]:
 		"""
 		ensures any input to be a string
 		"""
+
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
 
 		# any boolish false result = empty string
 		if not data:
@@ -30,18 +34,24 @@ class ContentClass(object):
 		return str(data)
 
 	@staticmethod
-	def asBoolean(data:Any) -> bool:
+	def asBoolean(data:Any, allow_null:bool = False) -> Optional[bool]:
 		"""
 		ensures any input to be a boolean
 		"""
 
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
+
 		return bool(data)
 
 	@staticmethod
-	def asInteger(data:Any) -> int:
+	def asInteger(data:Any, allow_null:bool = False) -> Optional[int]:
 		"""
 		ensures any input to be a integer
 		"""
+
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
 
 		try:
 			return int(data)
@@ -49,10 +59,13 @@ class ContentClass(object):
 			return 0
 
 	@staticmethod
-	def asFloat(data:Any) -> float:
+	def asFloat(data:Any, allow_null:bool = False) -> Optional[float]:
 		"""
 		ensures any input to be a float
 		"""
+
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
 
 		try:
 			return float(data)
@@ -60,10 +73,13 @@ class ContentClass(object):
 			return 0.0
 
 	@staticmethod
-	def asDatetime(data:Any, str_format:str="%Y-%m-%d %H:%M:%S") -> datetime:
+	def asDatetime(data:Any, str_format:str = "%Y-%m-%d %H:%M:%S", allow_null:bool = False) -> Optional[datetime]:
 		"""
 		ensures any input to be a datetime object
 		"""
+
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
 
 		try:
 			return datetime.strptime(data, str_format)
@@ -71,17 +87,34 @@ class ContentClass(object):
 			return datetime.strptime("2000-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
 
 	@staticmethod
-	def asList(data:Any) -> list:
+	def asList(data:Any, allow_null:bool = False) -> Optional[list]:
 		"""
 		tries to ensures any input to be a api save list
 		except that's impossible, so we just check if its a list,
 		or return a empty one.... whopsi
 		"""
 
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
+
 		if type(data) is list:
 			return data
 
 		return []
+
+	@classmethod
+	def asDecimal(cls, data:Any, allow_null:bool = False) -> Optional[Decimal]:
+		"""
+		ensures any input to be a float
+		"""
+
+		if allow_null and (data is None or type(data) is Undefined):
+			return None
+
+		try:
+			return Decimal(data)
+		except:
+			return Decimal(0)
 
 	@staticmethod
 	def fromJsonField(data:Union[str, bytes]) -> Union[dict, list]:
@@ -95,7 +128,7 @@ class ContentClass(object):
 		return json.loads(data)
 
 	@staticmethod
-	def fromStringList(data:Union[str, bytes], separator:str= ",") -> List[str]:
+	def fromStringList(data:Union[str, bytes], separator:str = ",") -> List[str]:
 		"""
 		splits string into a list,
 		giving undefined or bool(data) == False
