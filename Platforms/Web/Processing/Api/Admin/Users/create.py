a274a2dbaeadec005f02fdb9dc57d5e840c59016
The_CJ <christoperjaekel@googlemail.com>
2021-01-30 17:38:47 +0100

    small fix in create users admin

--- Platforms/Web/Processing/Api/Admin/Users/create.py
+++ Platforms/Web/Processing/Api/Admin/Users/create.py
@@ -1,55 +1,49 @@
 from typing import TYPE_CHECKING, List
 if TYPE_CHECKING:
-	from Platforms.Web.index import WebIndex
+	from Platforms.Web.main_web import PhaazebotWeb
 
 import json
-from aiohttp.web import Response, Request
-from Utils.Classes.authwebuser import AuthWebUser
+from aiohttp.web import Response
+from Utils.Classes.webuser import WebUser
+from Utils.Classes.extendedrequest import ExtendedRequest
 from Utils.Classes.webrequestcontent import WebRequestContent
+from Utils.Classes.storagetransformer import StorageTransformer
 from Utils.Classes.undefined import UNDEFINED
-from Utils.stringutils import passwordToHash as password_function
+from Utils.stringutils import passwordToHash as passwordFunction
 from Platforms.Web.db import getWebUsers
-from Platforms.Web.Processing.Api.errors import apiMissingData
-from Platforms.Web.Processing.Api.Account.errors import apiAccountTaken
 
-async def apiAdminUsersCreate(cls:"WebIndex", WebRequest:Request) -> Response:
+async def apiAdminUsersCreate(cls:"PhaazebotWeb", WebRequest:ExtendedRequest) -> Response:
 	"""
-		Default url: /api/admin/users/create
+	Default url: /api/admin/users/create
 	"""
 	Data:WebRequestContent = WebRequestContent(WebRequest)
 	await Data.load()
 
 	# get required stuff
-	username:str = Data.getStr("username", UNDEFINED, len_max=64)
-	email:str = Data.getStr("email", UNDEFINED, len_max=128)
-	password:str = Data.getStr("password", UNDEFINED, len_max=256)
+	Create:StorageTransformer = StorageTransformer()
+	Create["username"] = Data.getStr("username", UNDEFINED, len_max=64)
+	Create["email"] = Data.getStr("email", UNDEFINED, len_max=128)
+	Create["password"] = Data.getStr("password", UNDEFINED, len_max=256)
 
 	# checks
-	if not username or not email or not password:
-		return await apiMissingData(cls, WebRequest, msg="missing 'username', 'email' or 'password'")
+	if not Create["username"] or not Create["email"] or not Create["password"]:
+		return await cls.Tree.Api.errors.apiMissingData(cls, WebRequest, msg="missing 'username', 'email' or 'password'")
 
-	password = password_function(password)
+	Create["password"] = passwordFunction(Create["password"])
 
-	res_users:List[AuthWebUser] = await getWebUsers(cls,
-	                                                where="LOWER(`user`.`username`) = LOWER(%s) OR LOWER(`user`.`email`) = LOWER(%s)",
-	                                                where_values=(username, email)
-	                                                )
+	res_users:List[WebUser] = await getWebUsers(cls, overwrite_where=" AND LOWER(`user`.`username`) = LOWER(%s) OR LOWER(`user`.`email`) = LOWER(%s)", overwrite_where_values=(Create["username"], Create["email"]))
 
 	if res_users:
-		return await apiAccountTaken(cls, WebRequest)
+		return await cls.Tree.Api.Account.errors.apiAccountTaken(cls, WebRequest)
 
-	new_id:int = cls.Web.BASE.PhaazeDB.insertQuery(
-		table = "user",
-		content = dict(
-			username=username,
-			email=email,
-			password=password
-		)
+	new_id:int = cls.BASE.PhaazeDB.insertQuery(
+		table="user",
+		content=Create.getAllTransform()
 	)
 
-	cls.Web.BASE.Logger.debug(f"(API/Admin) Created user '{username}' (ID:{new_id})", require="api:user")
+	cls.BASE.Logger.debug(f"(API/Admin) Created user '{Create['username']}' (ID:{new_id})", require="api:user")
 	return cls.response(
-		text=json.dumps( dict(msg="successfull created user", user_id=new_id, status=200) ),
+		text=json.dumps(dict(msg="successful created user", user_id=new_id, status=200)),
 		content_type="application/json",
 		status=200
 	)
