f0eaff4bfbef6185b2aca85ade9f42179bd61ee2
The_CJ <christoperjaekel@googlemail.com>
2021-02-09 18:31:20 +0100

    more rewriting of discord api (regular)

--- Platforms/Web/Processing/Api/Discord/__init__.py
+++ Platforms/Web/Processing/Api/Discord/__init__.py
@@ -4,7 +4,7 @@ import Platforms.Web.Processing.Api.Discord.Assignroles as Assignroles
 # import Platforms.Web.Processing.Api.Discord.Levels as Levels
 # import Platforms.Web.Processing.Api.Discord.Logs as Logs
 import Platforms.Web.Processing.Api.Discord.Quotes as Quotes
-# import Platforms.Web.Processing.Api.Discord.Regulars as Regulars
+import Platforms.Web.Processing.Api.Discord.Regulars as Regulars
 import Platforms.Web.Processing.Api.Discord.Twitchalerts as Twitchalerts
 
 import Platforms.Web.Processing.Api.Discord.errors as errors
