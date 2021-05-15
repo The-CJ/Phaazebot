0528fabbebd7bf745190d8ca9efd84d512503685
The_CJ <christoperjaekel@googlemail.com>
2021-05-15 19:32:24 +0200

    more stuff for location redirecting

--- Platforms/Web/Content/Js/site-specific/twitch/main.js
+++ Platforms/Web/Content/Js/site-specific/twitch/main.js
@@ -7,9 +7,16 @@ function checkUserInput() {
 	};
 	$.get("/api/twitch/search", req)
 		.done(function(data) {
-			console.log(data);
+			$("#search_result_failed").hide();
+			$("#search_result_success").show();
+			var target_href = "/twitch/view/"+data.result.id;
+			var anchor = $("#search_result_success_anchor");
+			anchor.text(`${data.result.display_name + '\'s'} view page`);
+			anchor.attr("href", target_href);
+			window.location.href = target_href;
 		})
 		.fail(function(data) {
+			$("#search_result_success").hide();
 			$("#search_result_failed").show();
 			setTimeout(
 				() => { $("#search_result_failed").hide(); },
